e44e309b56276ffbc8c0c636dec36ea94962ff23
lizhengxiang <yitian@genaro.network>
2017-12-07 03:10:02 +0800

    update

--- accounts/abi/type_test.go
+++ accounts/abi/type_test.go
@@ -22,7 +22,7 @@ import (
 	"testing"
 
 	"github.com/davecgh/go-spew/spew"
-	"github.com/GenaroNetwork/Genaro-Core/common"
+	"github.com/GenaroNetwork/GenaroCore/common"
 )
 
 // typeWithoutStringer is a alias for the Type type which simply doesn't implement
