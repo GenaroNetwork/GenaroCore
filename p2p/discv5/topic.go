f4c0313ad4900540e40c8ef7dce1722cf2d29292
lizhengxiang <530997877@qq.com>
2017-12-07 03:10:01 +0800

    update

--- p2p/discv5/topic.go
+++ p2p/discv5/topic.go
@@ -23,8 +23,8 @@ import (
 	"math/rand"
 	"time"
 
-	"github.com/GenaroNetwork/Genaro-Core/common/mclock"
-	"github.com/GenaroNetwork/Genaro-Core/log"
+	"github.com/GenaroNetwork/GenaroCore/common/mclock"
+	"github.com/GenaroNetwork/GenaroCore/log"
 )
 
 const (
