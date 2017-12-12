a9b7d50f4677917ba02c13e955a98ec4686de7fd
lizhengxiang <sophia.littlefox@gmail.com>
2017-12-13 03:10:01 +0800

    update

--- trie/secure_trie_test.go
+++ trie/secure_trie_test.go
@@ -22,9 +22,9 @@ import (
 	"sync"
 	"testing"
 
-	"github.com/GenaroNetwork/Genaro-Core/common"
-	"github.com/GenaroNetwork/Genaro-Core/crypto"
-	"github.com/GenaroNetwork/Genaro-Core/ethdb"
+	"github.com/GenaroNetwork/GenaroCore/common"
+	"github.com/GenaroNetwork/GenaroCore/crypto"
+	"github.com/GenaroNetwork/GenaroCore/ethdb"
 )
 
 func newEmptySecure() *SecureTrie {
