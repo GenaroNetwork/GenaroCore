0483496b8c7cffb9b83deeb253ba761d089be309
lizhengxiang <334506638@qq.com>
2017-12-14 03:10:02 +0800

    update

--- core/asm/asm.go
+++ core/asm/asm.go
@@ -21,7 +21,7 @@ import (
 	"encoding/hex"
 	"fmt"
 
-	"github.com/GenaroNetwork/Genaro-Core/core/vm"
+	"github.com/GenaroNetwork/GenaroCore/core/vm"
 )
 
 // Iterator for disassembled EVM instructions
