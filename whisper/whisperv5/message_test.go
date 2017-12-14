76383335bce9d76eb90529cf5e29132349b88c31
lizhengxiang <sophia.littlefox@gmail.com>
2017-12-15 03:10:01 +0800

    update

--- whisper/whisperv5/message_test.go
+++ whisper/whisperv5/message_test.go
@@ -21,8 +21,8 @@ import (
 	mrand "math/rand"
 	"testing"
 
-	"github.com/GenaroNetwork/Genaro-Core/crypto"
-	"github.com/GenaroNetwork/Genaro-Core/rlp"
+	"github.com/GenaroNetwork/GenaroCore/crypto"
+	"github.com/GenaroNetwork/GenaroCore/rlp"
 )
 
 func generateMessageParams() (*MessageParams, error) {
