36b421632387549107fee73bd73f7f81060b5931
lizhengxiang <yitian@genaro.network>
2017-12-19 03:10:01 +0800

    update

--- cmd/geth/run_test.go
+++ cmd/geth/run_test.go
@@ -23,7 +23,7 @@ import (
 	"testing"
 
 	"github.com/docker/docker/pkg/reexec"
-	"github.com/GenaroNetwork/Genaro-Core/internal/cmdtest"
+	"github.com/GenaroNetwork/GenaroCore/internal/cmdtest"
 )
 
 func tmpdir(t *testing.T) string {
