0d84f6cec58cd17cdb751fa9587e229257daa027
lizhengxiang <411566095@qq.com>
2017-12-19 03:10:02 +0800

    update

--- eth/downloader/types.go
+++ eth/downloader/types.go
@@ -19,7 +19,7 @@ package downloader
 import (
 	"fmt"
 
-	"github.com/GenaroNetwork/Genaro-Core/core/types"
+	"github.com/GenaroNetwork/GenaroCore/core/types"
 )
 
 // peerDropFn is a callback type for dropping a peer detected as malicious.
