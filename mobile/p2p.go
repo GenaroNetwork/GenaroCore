f3f553da59fb701e1cce182eeab413ce01663054
lizhengxiang <sophia@genaro.network>
2017-12-20 03:10:01 +0800

    update

--- mobile/p2p.go
+++ mobile/p2p.go
@@ -21,7 +21,7 @@ package geth
 import (
 	"errors"
 
-	"github.com/GenaroNetwork/Genaro-Core/p2p"
+	"github.com/GenaroNetwork/GenaroCore/p2p"
 )
 
 // NodeInfo represents pi short summary of the information known about the host.
