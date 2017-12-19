9c9d81b1d5d691aa241548dff53402598eeb27d4
lizhengxiang <530997877@qq.com>
2017-12-20 03:10:01 +0800

    update

--- swarm/dev/run.sh
+++ swarm/dev/run.sh
@@ -80,7 +80,7 @@ run_image() {
     --rm \
     --hostname "${name}" \
     --name     "${name}" \
-    --volume   "${ROOT}:/go/src/github.com/GenaroNetwork/Genaro-Core" \
+    --volume   "${ROOT}:/go/src/github.com/GenaroNetwork/GenaroCore" \
     --volume   "/var/run/docker.sock:/var/run/docker.sock" \
     ${docker_args} \
     "${name}" \
