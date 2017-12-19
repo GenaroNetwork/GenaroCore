ee2eec5773d57f382009235bbd94ba669d26f656
lizhengxiang <sophia.littlefox@gmail.com>
2017-12-20 03:10:01 +0800

    update

--- whisper/whisperv6/topic.go
+++ whisper/whisperv6/topic.go
@@ -19,8 +19,8 @@
 package whisperv6
 
 import (
-	"github.com/GenaroNetwork/Genaro-Core/common"
-	"github.com/GenaroNetwork/Genaro-Core/common/hexutil"
+	"github.com/GenaroNetwork/GenaroCore/common"
+	"github.com/GenaroNetwork/GenaroCore/common/hexutil"
 )
 
 // TopicType represents a cryptographically secure, probabilistic partial
