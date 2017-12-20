9e54b6efdfc235fcb42e29a7df305e8a0c34796f
lizhengxiang <334506638@qq.com>
2017-12-21 03:10:02 +0800

    update

--- core/state/iterator_test.go
+++ core/state/iterator_test.go
@@ -20,8 +20,8 @@ import (
 	"bytes"
 	"testing"
 
-	"github.com/GenaroNetwork/Genaro-Core/common"
-	"github.com/GenaroNetwork/Genaro-Core/ethdb"
+	"github.com/GenaroNetwork/GenaroCore/common"
+	"github.com/GenaroNetwork/GenaroCore/ethdb"
 )
 
 // Tests that the node iterator indeed walks over the entire database contents.
