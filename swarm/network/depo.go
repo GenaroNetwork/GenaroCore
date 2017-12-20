435a8c93faab57211bc9989c14178c436abf9b76
lizhengxiang <530997877@qq.com>
2017-12-21 03:10:01 +0800

    update

--- swarm/network/depo.go
+++ swarm/network/depo.go
@@ -22,9 +22,9 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/GenaroNetwork/Genaro-Core/log"
-	"github.com/GenaroNetwork/Genaro-Core/metrics"
-	"github.com/GenaroNetwork/Genaro-Core/swarm/storage"
+	"github.com/GenaroNetwork/GenaroCore/log"
+	"github.com/GenaroNetwork/GenaroCore/metrics"
+	"github.com/GenaroNetwork/GenaroCore/swarm/storage"
 )
 
 //metrics variables
