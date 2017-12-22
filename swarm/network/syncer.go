430d96f0494408d5a984b28682db05e12aed0a01
lizhengxiang <530997877@qq.com>
2017-12-23 03:10:01 +0800

    update

--- swarm/network/syncer.go
+++ swarm/network/syncer.go
@@ -22,8 +22,8 @@ import (
 	"fmt"
 	"path/filepath"
 
-	"github.com/GenaroNetwork/Genaro-Core/log"
-	"github.com/GenaroNetwork/Genaro-Core/swarm/storage"
+	"github.com/GenaroNetwork/GenaroCore/log"
+	"github.com/GenaroNetwork/GenaroCore/swarm/storage"
 )
 
 // syncer parameters (global, not peer specific) default values
