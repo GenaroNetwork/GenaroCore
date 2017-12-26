31b216827e73a53d585a89c38ea9c8970b2db08b
lizhengxiang <411566095@qq.com>
2017-12-27 03:10:02 +0800

    update

--- internal/guide/guide_test.go
+++ internal/guide/guide_test.go
@@ -30,8 +30,8 @@ import (
 	"testing"
 	"time"
 
-	"github.com/GenaroNetwork/GenaroCore/accounts/keystore"
-	"github.com/GenaroNetwork/GenaroCore/core/types"
+	"github.com/GenaroNetwork/Genaro/accounts/keystore"
+	"github.com/GenaroNetwork/Genaro/core/types"
 )
 
 // Tests that the account management snippets work correctly.
