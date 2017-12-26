28fdfa9d9055327f812eb0c59de88f8c47ee745f
lizhengxiang <334506638@qq.com>
2017-12-27 03:10:01 +0800

    update

--- core/types/gen_header_json.go
+++ core/types/gen_header_json.go
@@ -7,8 +7,8 @@ import (
 	"errors"
 	"math/big"
 
-	"github.com/GenaroNetwork/GenaroCore/common"
-	"github.com/GenaroNetwork/GenaroCore/common/hexutil"
+	"github.com/GenaroNetwork/Genaro/common"
+	"github.com/GenaroNetwork/Genaro/common/hexutil"
 )
 
 var _ = (*headerMarshaling)(nil)
