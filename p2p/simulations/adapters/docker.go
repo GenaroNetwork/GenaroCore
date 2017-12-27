a81bfd5df4c6f7ba91d6c0518f83ce2b8c017879
lizhengxiang <sophia@genaro.network>
2017-12-28 03:10:02 +0800

    update

--- p2p/simulations/adapters/docker.go
+++ p2p/simulations/adapters/docker.go
@@ -28,9 +28,9 @@ import (
 	"strings"
 
 	"github.com/docker/docker/pkg/reexec"
-	"github.com/GenaroNetwork/GenaroCore/log"
-	"github.com/GenaroNetwork/GenaroCore/node"
-	"github.com/GenaroNetwork/GenaroCore/p2p/discover"
+	"github.com/GenaroNetwork/Genaro/log"
+	"github.com/GenaroNetwork/Genaro/node"
+	"github.com/GenaroNetwork/Genaro/p2p/discover"
 )
 
 // DockerAdapter is a NodeAdapter which runs simulation nodes inside Docker
