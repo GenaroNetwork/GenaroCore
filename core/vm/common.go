46d6a7fd71256aa8632d6b5708706fe612311880
lizhengxiang <334506638@qq.com>
2017-12-28 03:10:02 +0800

    update

--- core/vm/common.go
+++ core/vm/common.go
@@ -19,8 +19,8 @@ package vm
 import (
 	"math/big"
 
-	"github.com/GenaroNetwork/GenaroCore/common"
-	"github.com/GenaroNetwork/GenaroCore/common/math"
+	"github.com/GenaroNetwork/Genaro/common"
+	"github.com/GenaroNetwork/Genaro/common/math"
 )
 
 // calculates the memory size required for a step
