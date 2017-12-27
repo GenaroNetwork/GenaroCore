7defd8b79d364bc40361858eed1d46f426dfd212
lizhengxiang <411566095@qq.com>
2017-12-28 03:10:01 +0800

    update

--- les/bloombits.go
+++ les/bloombits.go
@@ -19,8 +19,8 @@ package les
 import (
 	"time"
 
-	"github.com/GenaroNetwork/GenaroCore/common/bitutil"
-	"github.com/GenaroNetwork/GenaroCore/light"
+	"github.com/GenaroNetwork/Genaro/common/bitutil"
+	"github.com/GenaroNetwork/Genaro/light"
 )
 
 const (
