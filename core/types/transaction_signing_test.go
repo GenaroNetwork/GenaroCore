f876d6d53e23d9932b43b82a64add9029f84b118
lizhengxiang <334506638@qq.com>
2017-12-28 03:10:01 +0800

    update

--- core/types/transaction_signing_test.go
+++ core/types/transaction_signing_test.go
@@ -20,9 +20,9 @@ import (
 	"math/big"
 	"testing"
 
-	"github.com/GenaroNetwork/GenaroCore/common"
-	"github.com/GenaroNetwork/GenaroCore/crypto"
-	"github.com/GenaroNetwork/GenaroCore/rlp"
+	"github.com/GenaroNetwork/Genaro/common"
+	"github.com/GenaroNetwork/Genaro/crypto"
+	"github.com/GenaroNetwork/Genaro/rlp"
 )
 
 func TestEIP155Signing(t *testing.T) {
