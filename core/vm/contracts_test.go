078a854441d13ef9f4b46a280d3f1fa36dd83630
lizhengxiang <334506638@qq.com>
2017-12-29 03:10:01 +0800

    update

--- core/vm/contracts_test.go
+++ core/vm/contracts_test.go
@@ -21,7 +21,7 @@ import (
 	"math/big"
 	"testing"
 
-	"github.com/GenaroNetwork/GenaroCore/common"
+	"github.com/GenaroNetwork/Genaro/common"
 )
 
 // precompiledTest defines the input/output pairs for precompiled contract tests.
