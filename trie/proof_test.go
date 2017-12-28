a2236f49333a4e2ffa38f02576c7a192e5536041
lizhengxiang <530997877@qq.com>
2017-12-29 03:10:01 +0800

    update

--- trie/proof_test.go
+++ trie/proof_test.go
@@ -23,9 +23,9 @@ import (
 	"testing"
 	"time"
 
-	"github.com/GenaroNetwork/GenaroCore/common"
-	"github.com/GenaroNetwork/GenaroCore/crypto"
-	"github.com/GenaroNetwork/GenaroCore/ethdb"
+	"github.com/GenaroNetwork/Genaro/common"
+	"github.com/GenaroNetwork/Genaro/crypto"
+	"github.com/GenaroNetwork/Genaro/ethdb"
 )
 
 func init() {
