76ec59d14f4493b349d45394272ab3956e424b42
lizhengxiang <sophia@genaro.network>
2017-12-30 03:10:01 +0800

    update

--- p2p/simulations/simulation.go
+++ p2p/simulations/simulation.go
@@ -20,7 +20,7 @@ import (
 	"context"
 	"time"
 
-	"github.com/GenaroNetwork/GenaroCore/p2p/discover"
+	"github.com/GenaroNetwork/Genaro/p2p/discover"
 )
 
 // Simulation provides a framework for running actions in a simulated network
