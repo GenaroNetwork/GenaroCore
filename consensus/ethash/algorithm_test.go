a77bacea121c8def0278f27a69288407ceca168e
lizhengxiang <yitian@genaro.network>
2017-12-30 03:10:02 +0800

    update

--- consensus/ethash/algorithm_test.go
+++ consensus/ethash/algorithm_test.go
@@ -25,9 +25,9 @@ import (
 	"sync"
 	"testing"
 
-	"github.com/GenaroNetwork/GenaroCore/common"
-	"github.com/GenaroNetwork/GenaroCore/common/hexutil"
-	"github.com/GenaroNetwork/GenaroCore/core/types"
+	"github.com/GenaroNetwork/Genaro/common"
+	"github.com/GenaroNetwork/Genaro/common/hexutil"
+	"github.com/GenaroNetwork/Genaro/core/types"
 )
 
 // Tests whether the dataset size calculator works correctly by cross checking the
