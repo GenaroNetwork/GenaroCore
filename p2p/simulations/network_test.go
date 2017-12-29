5379282369ca47ab7a6dd234eb5bd2afafe5997d
lizhengxiang <sophia@genaro.network>
2017-12-30 03:10:01 +0800

    update

--- p2p/simulations/network_test.go
+++ p2p/simulations/network_test.go
@@ -22,8 +22,8 @@ import (
 	"testing"
 	"time"
 
-	"github.com/GenaroNetwork/GenaroCore/p2p/discover"
-	"github.com/GenaroNetwork/GenaroCore/p2p/simulations/adapters"
+	"github.com/GenaroNetwork/Genaro/p2p/discover"
+	"github.com/GenaroNetwork/Genaro/p2p/simulations/adapters"
 )
 
 // TestNetworkSimulation creates a multi-node simulation network with each node
