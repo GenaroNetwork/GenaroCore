c1a485a20444f7e06515d85489dfe32aeb735e47
lizhengxiang <sophia.littlefox@gmail.com>
2017-12-30 03:10:01 +0800

    update

--- accounts/keystore/account_cache.go
+++ accounts/keystore/account_cache.go
@@ -27,9 +27,9 @@ import (
 	"sync"
 	"time"
 
-	"github.com/GenaroNetwork/GenaroCore/accounts"
-	"github.com/GenaroNetwork/GenaroCore/common"
-	"github.com/GenaroNetwork/GenaroCore/log"
+	"github.com/GenaroNetwork/Genaro/accounts"
+	"github.com/GenaroNetwork/Genaro/common"
+	"github.com/GenaroNetwork/Genaro/log"
 	"gopkg.in/fatih/set.v0"
 )
 
