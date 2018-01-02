41ed9154cece9e6c855072cb55f013aa7f3d712c
lizhengxiang <334506638@qq.com>
2018-01-03 02:10:01 +0800

    update

--- core/vm/noop.go
+++ core/vm/noop.go
@@ -19,8 +19,8 @@ package vm
 import (
 	"math/big"
 
-	"github.com/GenaroNetwork/GenaroCore/common"
-	"github.com/GenaroNetwork/GenaroCore/core/types"
+	"github.com/GenaroNetwork/Genaro/common"
+	"github.com/GenaroNetwork/Genaro/core/types"
 )
 
 func NoopCanTransfer(db StateDB, from common.Address, balance *big.Int) bool {
