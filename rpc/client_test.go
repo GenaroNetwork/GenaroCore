616ac4f64bf2a18326c3ead9fcf11f6d3f76e9ac
lizhengxiang <sophia@genaro.network>
2018-01-03 02:10:02 +0800

    update

--- rpc/client_test.go
+++ rpc/client_test.go
@@ -31,7 +31,7 @@ import (
 	"time"
 
 	"github.com/davecgh/go-spew/spew"
-	"github.com/GenaroNetwork/GenaroCore/log"
+	"github.com/GenaroNetwork/Genaro/log"
 )
 
 func TestClientRequest(t *testing.T) {
