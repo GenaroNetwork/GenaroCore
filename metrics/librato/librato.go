27e1bae530188b31a76f94374d19baa80abccfc5
lizhengxiang <411566095@qq.com>
2018-01-05 03:10:01 +0800

    update

--- metrics/librato/librato.go
+++ metrics/librato/librato.go
@@ -7,7 +7,7 @@ import (
 	"regexp"
 	"time"
 
-	"github.com/GenaroNetwork/GenaroCore/metrics"
+	"github.com/GenaroNetwork/Genaro/metrics"
 )
 
 // a regexp for extracting the unit from time.Duration.String
