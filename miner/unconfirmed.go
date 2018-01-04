519485dec5ffab8301cb5c6fcb0ce2b7742a23c4
lizhengxiang <411566095@qq.com>
2018-01-05 03:10:02 +0800

    update

--- miner/unconfirmed.go
+++ miner/unconfirmed.go
@@ -20,9 +20,9 @@ import (
 	"container/ring"
 	"sync"
 
-	"github.com/GenaroNetwork/GenaroCore/common"
-	"github.com/GenaroNetwork/GenaroCore/core/types"
-	"github.com/GenaroNetwork/GenaroCore/log"
+	"github.com/GenaroNetwork/Genaro/common"
+	"github.com/GenaroNetwork/Genaro/core/types"
+	"github.com/GenaroNetwork/Genaro/log"
 )
 
 // headerRetriever is used by the unconfirmed block set to verify whether a previously
