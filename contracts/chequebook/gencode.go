e7b7c7a3a8b0e82f283872e1c816ee3546508802
lizhengxiang <yitian@genaro.network>
2018-01-05 03:10:01 +0800

    update

--- contracts/chequebook/gencode.go
+++ contracts/chequebook/gencode.go
@@ -25,11 +25,11 @@ import (
 	"io/ioutil"
 	"math/big"
 
-	"github.com/GenaroNetwork/GenaroCore/accounts/abi/bind"
-	"github.com/GenaroNetwork/GenaroCore/accounts/abi/bind/backends"
-	"github.com/GenaroNetwork/GenaroCore/contracts/chequebook/contract"
-	"github.com/GenaroNetwork/GenaroCore/core"
-	"github.com/GenaroNetwork/GenaroCore/crypto"
+	"github.com/GenaroNetwork/Genaro/accounts/abi/bind"
+	"github.com/GenaroNetwork/Genaro/accounts/abi/bind/backends"
+	"github.com/GenaroNetwork/Genaro/contracts/chequebook/contract"
+	"github.com/GenaroNetwork/Genaro/core"
+	"github.com/GenaroNetwork/Genaro/crypto"
 )
 
 var (
