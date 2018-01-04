7d330dda08c178d36f954614df35b810b678c172
lizhengxiang <sophia@genaro.network>
2018-01-05 03:10:01 +0800

    update

--- swarm/api/config_test.go
+++ swarm/api/config_test.go
@@ -20,8 +20,8 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/GenaroNetwork/GenaroCore/common"
-	"github.com/GenaroNetwork/GenaroCore/crypto"
+	"github.com/GenaroNetwork/Genaro/common"
+	"github.com/GenaroNetwork/Genaro/crypto"
 )
 
 func TestConfig(t *testing.T) {
