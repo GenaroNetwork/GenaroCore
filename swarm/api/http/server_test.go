a52026efdd71485d31ba54ec096ceaa31cecf66e
lizhengxiang <sophia@genaro.network>
2018-01-06 03:17:01 +0800

    update

--- swarm/api/http/server_test.go
+++ swarm/api/http/server_test.go
@@ -26,11 +26,11 @@ import (
 	"sync"
 	"testing"
 
-	"github.com/GenaroNetwork/GenaroCore/common"
-	"github.com/GenaroNetwork/GenaroCore/swarm/api"
-	swarm "github.com/GenaroNetwork/GenaroCore/swarm/api/client"
-	"github.com/GenaroNetwork/GenaroCore/swarm/storage"
-	"github.com/GenaroNetwork/GenaroCore/swarm/testutil"
+	"github.com/GenaroNetwork/Genaro/common"
+	"github.com/GenaroNetwork/Genaro/swarm/api"
+	swarm "github.com/GenaroNetwork/Genaro/swarm/api/client"
+	"github.com/GenaroNetwork/Genaro/swarm/storage"
+	"github.com/GenaroNetwork/Genaro/swarm/testutil"
 )
 
 func TestBzzGetPath(t *testing.T) {
