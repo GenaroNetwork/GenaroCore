29128cc96961fae5bcf4b4b4867abdff950450f0
lizhengxiang <411566095@qq.com>
2018-01-06 03:17:02 +0800

    update

--- mobile/big.go
+++ mobile/big.go
@@ -22,7 +22,7 @@ import (
 	"errors"
 	"math/big"
 
-	"github.com/GenaroNetwork/GenaroCore/common"
+	"github.com/GenaroNetwork/Genaro/common"
 )
 
 // A BigInt represents a signed multi-precision integer.
