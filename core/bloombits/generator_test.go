59a9a48ce405ae220516c6d017c5a12790c29b2d
lizhengxiang <yitian@genaro.network>
2018-01-08 09:09:02 +0800

    update

--- core/bloombits/generator_test.go
+++ core/bloombits/generator_test.go
@@ -21,7 +21,7 @@ import (
 	"math/rand"
 	"testing"
 
-	"github.com/GenaroNetwork/GenaroCore/core/types"
+	"github.com/GenaroNetwork/Genaro/core/types"
 )
 
 // Tests that batched bloom bits are correctly rotated from the input bloom
