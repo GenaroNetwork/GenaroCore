dc3792b7d1aa78e6c47e7bcd642451e704334b2c
lizhengxiang <sophia@genaro.network>
2018-01-09 09:09:03 +0800

    update

--- swarm/network/kademlia/address_test.go
+++ swarm/network/kademlia/address_test.go
@@ -21,7 +21,7 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/GenaroNetwork/GenaroCore/common"
+	"github.com/GenaroNetwork/Genaro/common"
 )
 
 func (Address) Generate(rand *rand.Rand, size int) reflect.Value {
