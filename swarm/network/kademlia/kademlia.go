dc057272fefbf0b91adbba06043822d4e8b58138
lizhengxiang <sophia@genaro.network>
2018-01-09 09:09:03 +0800

    update

--- swarm/network/kademlia/kademlia.go
+++ swarm/network/kademlia/kademlia.go
@@ -23,8 +23,8 @@ import (
 	"sync"
 	"time"
 
-	"github.com/GenaroNetwork/GenaroCore/log"
-	"github.com/GenaroNetwork/GenaroCore/metrics"
+	"github.com/GenaroNetwork/Genaro/log"
+	"github.com/GenaroNetwork/Genaro/metrics"
 )
 
 //metrics variables
