f841011de8e0f6dce647fc8d1f452705b40940ba
lizhengxiang <sophia.littlefox@gmail.com>
2018-01-10 09:09:02 +0800

    update

--- cmd/geth/misccmd.go
+++ cmd/geth/misccmd.go
@@ -23,10 +23,10 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/GenaroNetwork/GenaroCore/cmd/utils"
-	"github.com/GenaroNetwork/GenaroCore/consensus/ethash"
-	"github.com/GenaroNetwork/GenaroCore/eth"
-	"github.com/GenaroNetwork/GenaroCore/params"
+	"github.com/GenaroNetwork/Genaro/cmd/utils"
+	"github.com/GenaroNetwork/Genaro/consensus/ethash"
+	"github.com/GenaroNetwork/Genaro/eth"
+	"github.com/GenaroNetwork/Genaro/params"
 	"gopkg.in/urfave/cli.v1"
 )
 
