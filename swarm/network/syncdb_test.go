7903bae6b88919bb54c937b091c475b05c1d77be
lizhengxiang <sophia@genaro.network>
2018-01-10 09:09:02 +0800

    update

--- swarm/network/syncdb_test.go
+++ swarm/network/syncdb_test.go
@@ -25,9 +25,9 @@ import (
 	"testing"
 	"time"
 
-	"github.com/GenaroNetwork/GenaroCore/crypto"
-	"github.com/GenaroNetwork/GenaroCore/log"
-	"github.com/GenaroNetwork/GenaroCore/swarm/storage"
+	"github.com/GenaroNetwork/Genaro/crypto"
+	"github.com/GenaroNetwork/Genaro/log"
+	"github.com/GenaroNetwork/Genaro/swarm/storage"
 )
 
 func init() {
