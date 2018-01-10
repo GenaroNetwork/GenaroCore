f0b1789586be983dd19da39579048602bca54874
lizhengxiang <sophia@genaro.network>
2018-01-10 09:09:02 +0800

    update

--- swarm/network/syncdb.go
+++ swarm/network/syncdb.go
@@ -20,8 +20,8 @@ import (
 	"encoding/binary"
 	"fmt"
 
-	"github.com/GenaroNetwork/GenaroCore/log"
-	"github.com/GenaroNetwork/GenaroCore/swarm/storage"
+	"github.com/GenaroNetwork/Genaro/log"
+	"github.com/GenaroNetwork/Genaro/swarm/storage"
 	"github.com/syndtr/goleveldb/leveldb"
 	"github.com/syndtr/goleveldb/leveldb/iterator"
 )
