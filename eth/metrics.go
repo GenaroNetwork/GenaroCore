60b9849d219b84ce652a5dceaed02fc7f0d4ccfd
lizhengxiang <334506638@qq.com>
2018-01-11 09:09:02 +0800

    update

--- eth/metrics.go
+++ eth/metrics.go
@@ -17,8 +17,8 @@
 package eth
 
 import (
-	"github.com/GenaroNetwork/GenaroCore/metrics"
-	"github.com/GenaroNetwork/GenaroCore/p2p"
+	"github.com/GenaroNetwork/Genaro/metrics"
+	"github.com/GenaroNetwork/Genaro/p2p"
 )
 
 var (
