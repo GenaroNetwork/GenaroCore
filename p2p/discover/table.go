8dcc85fa78a14c2b0a97ebc76f4f0bd0fc1ead76
lizhengxiang <411566095@qq.com>
2018-01-11 09:09:03 +0800

    update

--- p2p/discover/table.go
+++ p2p/discover/table.go
@@ -33,10 +33,10 @@ import (
 	"sync"
 	"time"
 
-	"github.com/GenaroNetwork/GenaroCore/common"
-	"github.com/GenaroNetwork/GenaroCore/crypto"
-	"github.com/GenaroNetwork/GenaroCore/log"
-	"github.com/GenaroNetwork/GenaroCore/p2p/netutil"
+	"github.com/GenaroNetwork/Genaro/common"
+	"github.com/GenaroNetwork/Genaro/crypto"
+	"github.com/GenaroNetwork/Genaro/log"
+	"github.com/GenaroNetwork/Genaro/p2p/netutil"
 )
 
 const (
