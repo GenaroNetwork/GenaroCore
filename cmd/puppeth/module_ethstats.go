1fa5269fd08be8f5aee2639dc94fb5764d82160a
lizhengxiang <sophia.littlefox@gmail.com>
2018-01-11 09:09:02 +0800

    update

--- cmd/puppeth/module_ethstats.go
+++ cmd/puppeth/module_ethstats.go
@@ -25,7 +25,7 @@ import (
 	"strings"
 	"text/template"
 
-	"github.com/GenaroNetwork/GenaroCore/log"
+	"github.com/GenaroNetwork/Genaro/log"
 )
 
 // ethstatsDockerfile is the Dockerfile required to build an ethstats backend
