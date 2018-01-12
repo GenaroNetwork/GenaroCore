e907f801615f6e62c9606225274c54c2a8dc6011
lizhengxiang <334506638@qq.com>
2018-01-12 09:09:02 +0800

    update

--- eth/sync_test.go
+++ eth/sync_test.go
@@ -21,9 +21,9 @@ import (
 	"testing"
 	"time"
 
-	"github.com/GenaroNetwork/GenaroCore/eth/downloader"
-	"github.com/GenaroNetwork/GenaroCore/p2p"
-	"github.com/GenaroNetwork/GenaroCore/p2p/discover"
+	"github.com/GenaroNetwork/Genaro/eth/downloader"
+	"github.com/GenaroNetwork/Genaro/p2p"
+	"github.com/GenaroNetwork/Genaro/p2p/discover"
 )
 
 // Tests that fast sync gets disabled as soon as a real block is successfully
