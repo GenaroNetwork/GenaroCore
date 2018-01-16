25de9757dd1d8b3f062416ac22d4847909219782
lizhengxiang <sophia.littlefox@gmail.com>
2018-01-16 09:23:05 +0800

    update

--- cmd/puppeth/wizard_nginx.go
+++ cmd/puppeth/wizard_nginx.go
@@ -19,7 +19,7 @@ package main
 import (
 	"fmt"
 
-	"github.com/GenaroNetwork/GenaroCore/log"
+	"github.com/GenaroNetwork/Genaro/log"
 )
 
 // ensureVirtualHost checks whether a reverse-proxy is running on the specified
