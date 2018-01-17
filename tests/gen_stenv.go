119f0e81f21adad53f6bcc7b8f576f4b4813745e
lizhengxiang <sophia@genaro.network>
2018-01-17 09:23:05 +0800

    update

--- tests/gen_stenv.go
+++ tests/gen_stenv.go
@@ -7,8 +7,8 @@ import (
 	"errors"
 	"math/big"
 
-	"github.com/GenaroNetwork/Genaro/common"
-	"github.com/GenaroNetwork/Genaro/common/math"
+	"github.com/GenaroNetwork/GenaroGNX/common"
+	"github.com/GenaroNetwork/GenaroGNX/common/math"
 )
 
 var _ = (*stEnvMarshaling)(nil)
