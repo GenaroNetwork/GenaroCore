14072343987ab20474683001bf86025ee451f5bd
lizhengxiang <yitian@genaro.network>
2018-01-18 09:23:05 +0800

    update

--- internal/debug/trace.go
+++ internal/debug/trace.go
@@ -23,7 +23,7 @@ import (
 	"os"
 	"runtime/trace"
 
-	"github.com/GenaroNetwork/GenaroGNX/log"
+	"github.com/GenaroNetwork/Genaro/log"
 )
 
 // StartGoTrace turns on tracing, writing to the given file.
