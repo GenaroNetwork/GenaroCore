305e82ee2b39b6b260553f058af7c14f14c65b03
lizhengxiang <530997877@qq.com>
2018-01-19 09:23:06 +0800

    update

--- common/bitutil/compress_test.go
+++ common/bitutil/compress_test.go
@@ -21,7 +21,7 @@ import (
 	"math/rand"
 	"testing"
 
-	"github.com/GenaroNetwork/GenaroGNX/common/hexutil"
+	"github.com/GenaroNetwork/Genaro/common/hexutil"
 )
 
 // Tests that data bitset encoding and decoding works and is bijective.
