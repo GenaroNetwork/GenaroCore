6b19027ad8bfa7651519f415d99e313cfd630165
lizhengxiang <530997877@qq.com>
2018-01-19 09:23:07 +0800

    update

--- accounts/abi/method.go
+++ accounts/abi/method.go
@@ -20,7 +20,7 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/GenaroNetwork/GenaroGNX/crypto"
+	"github.com/GenaroNetwork/Genaro/crypto"
 )
 
 // Method represents a callable given a `Name` and whether the method is a constant.
