722a496030372355c674aa152e743045b75eb68f
lizhengxiang <sophia@genaro.network>
2018-01-19 09:23:08 +0800

    update

--- trie/hasher.go
+++ trie/hasher.go
@@ -21,9 +21,9 @@ import (
 	"hash"
 	"sync"
 
-	"github.com/GenaroNetwork/Genaro/common"
-	"github.com/GenaroNetwork/Genaro/crypto/sha3"
-	"github.com/GenaroNetwork/Genaro/rlp"
+	"github.com/GenaroNetwork/GenaroGNX/common"
+	"github.com/GenaroNetwork/GenaroGNX/crypto/sha3"
+	"github.com/GenaroNetwork/GenaroGNX/rlp"
 )
 
 type hasher struct {
