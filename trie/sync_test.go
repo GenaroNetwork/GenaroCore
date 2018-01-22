29432fb87013bc1f491da53582c3fc5a2d1c81fa
lizhengxiang <334506638@qq.com>
2018-01-22 20:00:10 +0800

    update

--- trie/sync_test.go
+++ trie/sync_test.go
@@ -20,8 +20,8 @@ import (
 	"bytes"
 	"testing"
 
-	"github.com/GenaroNetwork/GenaroGNX/common"
-	"github.com/GenaroNetwork/GenaroGNX/ethdb"
+	"github.com/GenaroNetwork/Genaro/common"
+	"github.com/GenaroNetwork/Genaro/ethdb"
 )
 
 // makeTestTrie create a sample test trie to test node-wise reconstruction.
