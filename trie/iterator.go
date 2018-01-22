16ec1a6ffee38dd9c15e502d5ea733d12e4e4dd1
lizhengxiang <sophia@genaro.network>
2018-01-22 20:00:04 +0800

    update

--- trie/iterator.go
+++ trie/iterator.go
@@ -21,7 +21,7 @@ import (
 	"container/heap"
 	"errors"
 
-	"github.com/GenaroNetwork/Genaro/common"
+	"github.com/GenaroNetwork/GenaroGNX/common"
 )
 
 // Iterator is a key-value trie iterator that traverses a Trie.
