55829f5bb0b128b906c62d74f4f1dd9f3d08691d
lizhengxiang <sophia.littlefox@gmail.com>
2018-01-22 20:00:04 +0800

    update

--- core/vm/contract.go
+++ core/vm/contract.go
@@ -19,7 +19,7 @@ package vm
 import (
 	"math/big"
 
-	"github.com/GenaroNetwork/GenaroGNX/common"
+	"github.com/GenaroNetwork/Genaro/common"
 )
 
 // ContractRef is a reference to the contract's backing object
