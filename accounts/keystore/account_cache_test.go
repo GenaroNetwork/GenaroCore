75a3751ab84e9e6342e4990ed46f9b37dc4f1f02
lizhengxiang <yitian@genaro.network>
2018-01-22 20:00:05 +0800

    update

--- accounts/keystore/account_cache_test.go
+++ accounts/keystore/account_cache_test.go
@@ -29,8 +29,8 @@ import (
 
 	"github.com/cespare/cp"
 	"github.com/davecgh/go-spew/spew"
-	"github.com/GenaroNetwork/Genaro/accounts"
-	"github.com/GenaroNetwork/Genaro/common"
+	"github.com/GenaroNetwork/GenaroGNX/accounts"
+	"github.com/GenaroNetwork/GenaroGNX/common"
 )
 
 var (
