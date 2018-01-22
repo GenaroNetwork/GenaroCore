4024a102cbf559e820127c9394a27ef6b4f064f6
lizhengxiang <sophia@genaro.network>
2018-01-22 20:00:07 +0800

    update

--- trie/secure_trie.go
+++ trie/secure_trie.go
@@ -19,8 +19,8 @@ package trie
 import (
 	"fmt"
 
-	"github.com/GenaroNetwork/Genaro/common"
-	"github.com/GenaroNetwork/Genaro/log"
+	"github.com/GenaroNetwork/GenaroGNX/common"
+	"github.com/GenaroNetwork/GenaroGNX/log"
 )
 
 // SecureTrie wraps a trie with key hashing. In a secure trie, all
