0d1997bbf3a9fcc091dcebac5589b8137b622d77
lizhengxiang <yitian@genaro.network>
2018-01-22 20:00:04 +0800

    update

--- accounts/abi/numbers.go
+++ accounts/abi/numbers.go
@@ -20,8 +20,8 @@ import (
 	"math/big"
 	"reflect"
 
-	"github.com/GenaroNetwork/Genaro/common"
-	"github.com/GenaroNetwork/Genaro/common/math"
+	"github.com/GenaroNetwork/GenaroGNX/common"
+	"github.com/GenaroNetwork/GenaroGNX/common/math"
 )
 
 var (
