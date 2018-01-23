bfd134d06f9488d2e0f7ffb47e16f1c4e2af5013
lizhengxiang <yitian@genaro.network>
2018-01-23 20:00:04 +0800

    update

--- p2p/simulations/network.go
+++ p2p/simulations/network.go
@@ -24,11 +24,11 @@ import (
 	"sync"
 	"time"
 
-	"github.com/GenaroNetwork/GenaroGNX/event"
-	"github.com/GenaroNetwork/GenaroGNX/log"
-	"github.com/GenaroNetwork/GenaroGNX/p2p"
-	"github.com/GenaroNetwork/GenaroGNX/p2p/discover"
-	"github.com/GenaroNetwork/GenaroGNX/p2p/simulations/adapters"
+	"github.com/GenaroNetwork/Genaro/event"
+	"github.com/GenaroNetwork/Genaro/log"
+	"github.com/GenaroNetwork/Genaro/p2p"
+	"github.com/GenaroNetwork/Genaro/p2p/discover"
+	"github.com/GenaroNetwork/Genaro/p2p/simulations/adapters"
 )
 
 var dialBanTimeout = 200 * time.Millisecond
