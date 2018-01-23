3975fcc7d8555634914015f6529583411ca72f45
lizhengxiang <530997877@qq.com>
2018-01-23 20:00:06 +0800

    update

--- consensus/ethash/consensus_test.go
+++ consensus/ethash/consensus_test.go
@@ -23,9 +23,9 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/GenaroNetwork/GenaroGNX/common/math"
-	"github.com/GenaroNetwork/GenaroGNX/core/types"
-	"github.com/GenaroNetwork/GenaroGNX/params"
+	"github.com/GenaroNetwork/Genaro/common/math"
+	"github.com/GenaroNetwork/Genaro/core/types"
+	"github.com/GenaroNetwork/Genaro/params"
 )
 
 type diffTest struct {
