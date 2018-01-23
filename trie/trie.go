9c65b55564aa8848b1daefdf1c049e5f9258a4b9
lizhengxiang <334506638@qq.com>
2018-01-23 20:00:04 +0800

    update

--- trie/trie.go
+++ trie/trie.go
@@ -21,10 +21,10 @@ import (
 	"bytes"
 	"fmt"
 
-	"github.com/GenaroNetwork/GenaroGNX/common"
-	"github.com/GenaroNetwork/GenaroGNX/crypto"
-	"github.com/GenaroNetwork/GenaroGNX/log"
-	"github.com/GenaroNetwork/GenaroGNX/metrics"
+	"github.com/GenaroNetwork/Genaro/common"
+	"github.com/GenaroNetwork/Genaro/crypto"
+	"github.com/GenaroNetwork/Genaro/log"
+	"github.com/GenaroNetwork/Genaro/metrics"
 )
 
 var (
