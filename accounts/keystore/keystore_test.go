9db2ebb3d60d4d46167fa52146934cfb60f5256a
lizhengxiang <530997877@qq.com>
2018-01-24 20:00:04 +0800

    update

--- accounts/keystore/keystore_test.go
+++ accounts/keystore/keystore_test.go
@@ -26,9 +26,9 @@ import (
 	"testing"
 	"time"
 
-	"github.com/GenaroNetwork/GenaroGNX/accounts"
-	"github.com/GenaroNetwork/GenaroGNX/common"
-	"github.com/GenaroNetwork/GenaroGNX/event"
+	"github.com/GenaroNetwork/Genaro/accounts"
+	"github.com/GenaroNetwork/Genaro/common"
+	"github.com/GenaroNetwork/Genaro/event"
 )
 
 var testSigData = make([]byte, 32)
