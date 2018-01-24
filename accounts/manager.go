bb2e75cb602d7691b9ca80beccf414327d1e298b
lizhengxiang <yitian@genaro.network>
2018-01-24 20:00:05 +0800

    update

--- accounts/manager.go
+++ accounts/manager.go
@@ -21,7 +21,7 @@ import (
 	"sort"
 	"sync"
 
-	"github.com/GenaroNetwork/Genaro/event"
+	"github.com/GenaroNetwork/GenaroGNX/event"
 )
 
 // Manager is an overarching account manager that can communicate with various
