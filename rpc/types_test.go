2ec282b0a265db4ac3d49ab5269ae9649923e35f
lizhengxiang <yitian@genaro.network>
2018-01-24 20:00:09 +0800

    update

--- rpc/types_test.go
+++ rpc/types_test.go
@@ -20,7 +20,7 @@ import (
 	"encoding/json"
 	"testing"
 
-	"github.com/GenaroNetwork/GenaroGNX/common/math"
+	"github.com/GenaroNetwork/Genaro/common/math"
 )
 
 func TestBlockNumberJSONUnmarshal(t *testing.T) {
