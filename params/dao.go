cbaa6593c97f778d1d2deff6f2ff85f4387c8531
lizhengxiang <411566095@qq.com>
2018-01-24 20:00:04 +0800

    update

--- params/dao.go
+++ params/dao.go
@@ -19,7 +19,7 @@ package params
 import (
 	"math/big"
 
-	"github.com/GenaroNetwork/Genaro/common"
+	"github.com/GenaroNetwork/GenaroGNX/common"
 )
 
 // DAOForkBlockExtra is the block header extra-data field to set for the DAO fork
