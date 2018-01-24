1467d16eb8982817a29f2edc9ade47121388cd4c
lizhengxiang <334506638@qq.com>
2018-01-24 20:00:06 +0800

    update

--- light/odr.go
+++ light/odr.go
@@ -22,10 +22,10 @@ import (
 	"context"
 	"math/big"
 
-	"github.com/GenaroNetwork/Genaro/common"
-	"github.com/GenaroNetwork/Genaro/core"
-	"github.com/GenaroNetwork/Genaro/core/types"
-	"github.com/GenaroNetwork/Genaro/ethdb"
+	"github.com/GenaroNetwork/GenaroGNX/common"
+	"github.com/GenaroNetwork/GenaroGNX/core"
+	"github.com/GenaroNetwork/GenaroGNX/core/types"
+	"github.com/GenaroNetwork/GenaroGNX/ethdb"
 )
 
 // NoOdr is the default context passed to an ODR capable function when the ODR
