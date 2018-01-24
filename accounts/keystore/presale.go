f7a9c5637f6af15c2f358b2f0ef12f6cb60ec36e
lizhengxiang <yitian@genaro.network>
2018-01-24 20:00:04 +0800

    update

--- accounts/keystore/presale.go
+++ accounts/keystore/presale.go
@@ -25,8 +25,8 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/GenaroNetwork/Genaro/accounts"
-	"github.com/GenaroNetwork/Genaro/crypto"
+	"github.com/GenaroNetwork/GenaroGNX/accounts"
+	"github.com/GenaroNetwork/GenaroGNX/crypto"
 	"github.com/pborman/uuid"
 	"golang.org/x/crypto/pbkdf2"
 )
