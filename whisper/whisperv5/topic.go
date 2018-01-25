15fe767ccc27d1c4f5ae422e90a9029fe79d53b2
lizhengxiang <sophia@genaro.network>
2018-01-25 20:00:05 +0800

    update

--- whisper/whisperv5/topic.go
+++ whisper/whisperv5/topic.go
@@ -19,8 +19,8 @@
 package whisperv5
 
 import (
-	"github.com/GenaroNetwork/Genaro/common"
-	"github.com/GenaroNetwork/Genaro/common/hexutil"
+	"github.com/GenaroNetwork/GenaroGNX/common"
+	"github.com/GenaroNetwork/GenaroGNX/common/hexutil"
 )
 
 // Topic represents a cryptographically secure, probabilistic partial
