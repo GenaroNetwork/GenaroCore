658a8417cb7531d8fed6ca2525ad9d129219f7e8
lizhengxiang <yitian@genaro.network>
2018-01-26 20:00:08 +0800

    update

--- cmd/evm/internal/compiler/compiler.go
+++ cmd/evm/internal/compiler/compiler.go
@@ -20,7 +20,7 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/GenaroNetwork/Genaro/core/asm"
+	"github.com/GenaroNetwork/GenaroGNX/core/asm"
 )
 
 func Compile(fn string, src []byte, debug bool) (string, error) {
