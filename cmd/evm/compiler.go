7068266f6afda322cb4b3b55171eb7c8c3414ed2
lizhengxiang <yitian@genaro.network>
2018-01-26 20:00:08 +0800

    update

--- cmd/evm/compiler.go
+++ cmd/evm/compiler.go
@@ -21,7 +21,7 @@ import (
 	"fmt"
 	"io/ioutil"
 
-	"github.com/GenaroNetwork/Genaro/cmd/evm/internal/compiler"
+	"github.com/GenaroNetwork/GenaroGNX/cmd/evm/internal/compiler"
 
 	cli "gopkg.in/urfave/cli.v1"
 )
