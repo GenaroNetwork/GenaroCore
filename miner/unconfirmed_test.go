3ccf7e0a48ca5cec5c46f9e24238431491f0b520
lizhengxiang <yitian@genaro.network>
2018-01-26 20:00:11 +0800

    update

--- miner/unconfirmed_test.go
+++ miner/unconfirmed_test.go
@@ -19,8 +19,8 @@ package miner
 import (
 	"testing"
 
-	"github.com/GenaroNetwork/GenaroGNX/common"
-	"github.com/GenaroNetwork/GenaroGNX/core/types"
+	"github.com/GenaroNetwork/Genaro/common"
+	"github.com/GenaroNetwork/Genaro/core/types"
 )
 
 // noopHeaderRetriever is an implementation of headerRetriever that always
