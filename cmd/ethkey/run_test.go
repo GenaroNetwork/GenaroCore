61ce288b19a7dfd3dce2b4a5eb99f5c0690af23a
lizhengxiang <yitian@genaro.network>
2018-01-26 20:00:08 +0800

    update

--- cmd/ethkey/run_test.go
+++ cmd/ethkey/run_test.go
@@ -22,7 +22,7 @@ import (
 	"testing"
 
 	"github.com/docker/docker/pkg/reexec"
-	"github.com/GenaroNetwork/Genaro/internal/cmdtest"
+	"github.com/GenaroNetwork/GenaroGNX/internal/cmdtest"
 )
 
 type testEthkey struct {
