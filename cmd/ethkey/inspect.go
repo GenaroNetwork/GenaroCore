8cef4269626b2fccc8ee5f6de8e739870babcbdf
lizhengxiang <530997877@qq.com>
2018-01-26 20:00:08 +0800

    update

--- cmd/ethkey/inspect.go
+++ cmd/ethkey/inspect.go
@@ -21,9 +21,9 @@ import (
 	"fmt"
 	"io/ioutil"
 
-	"github.com/GenaroNetwork/GenaroGNX/accounts/keystore"
-	"github.com/GenaroNetwork/GenaroGNX/cmd/utils"
-	"github.com/GenaroNetwork/GenaroGNX/crypto"
+	"github.com/GenaroNetwork/Genaro/accounts/keystore"
+	"github.com/GenaroNetwork/Genaro/cmd/utils"
+	"github.com/GenaroNetwork/Genaro/crypto"
 	"gopkg.in/urfave/cli.v1"
 )
 
