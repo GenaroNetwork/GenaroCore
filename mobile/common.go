fab7c3c2d0b454bc53b0cab47832f0c4085d2939
lizhengxiang <yitian@genaro.network>
2018-01-29 20:00:05 +0800

    update

--- mobile/common.go
+++ mobile/common.go
@@ -24,7 +24,7 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/GenaroNetwork/GenaroGNX/common"
+	"github.com/GenaroNetwork/Genaro/common"
 )
 
 // Hash represents the 32 byte Keccak256 hash of arbitrary data.
