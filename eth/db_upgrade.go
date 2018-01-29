ea56b3f72194bd43685d1b784cf27bad4bd874f2
lizhengxiang <334506638@qq.com>
2018-01-29 20:00:04 +0800

    update

--- eth/db_upgrade.go
+++ eth/db_upgrade.go
@@ -21,11 +21,11 @@ import (
 	"bytes"
 	"time"
 
-	"github.com/GenaroNetwork/Genaro/common"
-	"github.com/GenaroNetwork/Genaro/core"
-	"github.com/GenaroNetwork/Genaro/ethdb"
-	"github.com/GenaroNetwork/Genaro/log"
-	"github.com/GenaroNetwork/Genaro/rlp"
+	"github.com/GenaroNetwork/GenaroGNX/common"
+	"github.com/GenaroNetwork/GenaroGNX/core"
+	"github.com/GenaroNetwork/GenaroGNX/ethdb"
+	"github.com/GenaroNetwork/GenaroGNX/log"
+	"github.com/GenaroNetwork/GenaroGNX/rlp"
 )
 
 var deduplicateData = []byte("dbUpgrade_20170714deduplicateData")
