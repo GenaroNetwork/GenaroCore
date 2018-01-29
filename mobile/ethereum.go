f18a3cacf4d42a1565977d94c3b5e79f1517ed29
lizhengxiang <yitian@genaro.network>
2018-01-29 20:00:07 +0800

    update

--- mobile/ethereum.go
+++ mobile/ethereum.go
@@ -21,8 +21,8 @@ package geth
 import (
 	"errors"
 
-	ethereum "github.com/GenaroNetwork/GenaroGNX"
-	"github.com/GenaroNetwork/GenaroGNX/common"
+	ethereum "github.com/GenaroNetwork/Genaro"
+	"github.com/GenaroNetwork/Genaro/common"
 )
 
 // Subscription represents an event subscription where events are
