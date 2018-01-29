7f9a93e7216105774f6b72913dc4610d25aed268
lizhengxiang <411566095@qq.com>
2018-01-29 20:00:07 +0800

    update

--- swarm/fuse/fuse_file.go
+++ swarm/fuse/fuse_file.go
@@ -26,8 +26,8 @@ import (
 
 	"bazil.org/fuse"
 	"bazil.org/fuse/fs"
-	"github.com/GenaroNetwork/Genaro/log"
-	"github.com/GenaroNetwork/Genaro/swarm/storage"
+	"github.com/GenaroNetwork/GenaroGNX/log"
+	"github.com/GenaroNetwork/GenaroGNX/swarm/storage"
 	"golang.org/x/net/context"
 )
 
