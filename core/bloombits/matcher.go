3e3aacd77a34613b42fa3e7551501ae5f9ff9b3d
lizhengxiang <530997877@qq.com>
2018-01-29 20:00:06 +0800

    update

--- core/bloombits/matcher.go
+++ core/bloombits/matcher.go
@@ -26,8 +26,8 @@ import (
 	"sync/atomic"
 	"time"
 
-	"github.com/GenaroNetwork/GenaroGNX/common/bitutil"
-	"github.com/GenaroNetwork/GenaroGNX/crypto"
+	"github.com/GenaroNetwork/Genaro/common/bitutil"
+	"github.com/GenaroNetwork/Genaro/crypto"
 )
 
 // bloomIndexes represents the bit indexes inside the bloom filter that belong
