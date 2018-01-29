1a3acf8c92e7f79c358fe751b1c318f15d3c625b
lizhengxiang <yitian@genaro.network>
2018-01-29 20:00:07 +0800

    update

--- mobile/ethclient.go
+++ mobile/ethclient.go
@@ -21,8 +21,8 @@ package geth
 import (
 	"math/big"
 
-	"github.com/GenaroNetwork/GenaroGNX/core/types"
-	"github.com/GenaroNetwork/GenaroGNX/ethclient"
+	"github.com/GenaroNetwork/Genaro/core/types"
+	"github.com/GenaroNetwork/Genaro/ethclient"
 )
 
 // EthereumClient provides access to the Ethereum APIs.
