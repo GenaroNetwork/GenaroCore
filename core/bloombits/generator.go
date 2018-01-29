4d4a8aa23b65ec76a051603fcd81d97847bf5d3f
lizhengxiang <yitian@genaro.network>
2018-01-29 20:00:04 +0800

    update

--- core/bloombits/generator.go
+++ core/bloombits/generator.go
@@ -19,7 +19,7 @@ package bloombits
 import (
 	"errors"
 
-	"github.com/GenaroNetwork/Genaro/core/types"
+	"github.com/GenaroNetwork/GenaroGNX/core/types"
 )
 
 // errSectionOutOfBounds is returned if the user tried to add more bloom filters
