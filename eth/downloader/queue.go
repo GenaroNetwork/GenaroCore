3b232a297092f2cab46085c7876b90c1247a41e5
lizhengxiang <sophia.littlefox@gmail.com>
2018-01-30 20:00:05 +0800

    update

--- eth/downloader/queue.go
+++ eth/downloader/queue.go
@@ -25,10 +25,10 @@ import (
 	"sync"
 	"time"
 
-	"github.com/GenaroNetwork/GenaroGNX/common"
-	"github.com/GenaroNetwork/GenaroGNX/core/types"
-	"github.com/GenaroNetwork/GenaroGNX/log"
-	"github.com/GenaroNetwork/GenaroGNX/metrics"
+	"github.com/GenaroNetwork/Genaro/common"
+	"github.com/GenaroNetwork/Genaro/core/types"
+	"github.com/GenaroNetwork/Genaro/log"
+	"github.com/GenaroNetwork/Genaro/metrics"
 	"gopkg.in/karalabe/cookiejar.v2/collections/prque"
 )
 
