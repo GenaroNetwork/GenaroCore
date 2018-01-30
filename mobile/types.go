d3b44e9c6d2ed50f1b34f1d182d3cd645421c671
lizhengxiang <yitian@genaro.network>
2018-01-30 20:00:08 +0800

    update

--- mobile/types.go
+++ mobile/types.go
@@ -23,9 +23,9 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/GenaroNetwork/GenaroGNX/common"
-	"github.com/GenaroNetwork/GenaroGNX/core/types"
-	"github.com/GenaroNetwork/GenaroGNX/rlp"
+	"github.com/GenaroNetwork/Genaro/common"
+	"github.com/GenaroNetwork/Genaro/core/types"
+	"github.com/GenaroNetwork/Genaro/rlp"
 )
 
 // A Nonce is a 64-bit hash which proves (combined with the mix-hash) that
