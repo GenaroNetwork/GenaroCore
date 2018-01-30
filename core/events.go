a707e68033ea7fc1bba99f68dc8d9f8c96552f1f
lizhengxiang <yitian@genaro.network>
2018-01-30 20:00:05 +0800

    update

--- core/events.go
+++ core/events.go
@@ -17,8 +17,8 @@
 package core
 
 import (
-	"github.com/GenaroNetwork/Genaro/common"
-	"github.com/GenaroNetwork/Genaro/core/types"
+	"github.com/GenaroNetwork/GenaroGNX/common"
+	"github.com/GenaroNetwork/GenaroGNX/core/types"
 )
 
 // TxPreEvent is posted when a transaction enters the transaction pool.
