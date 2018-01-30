1b9118b29943cf758b8ea89f6fcaa11f755bb196
lizhengxiang <530997877@qq.com>
2018-01-30 20:00:08 +0800

    update

--- cmd/geth/dao_test.go
+++ cmd/geth/dao_test.go
@@ -23,10 +23,10 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/GenaroNetwork/GenaroGNX/common"
-	"github.com/GenaroNetwork/GenaroGNX/core"
-	"github.com/GenaroNetwork/GenaroGNX/ethdb"
-	"github.com/GenaroNetwork/GenaroGNX/params"
+	"github.com/GenaroNetwork/Genaro/common"
+	"github.com/GenaroNetwork/Genaro/core"
+	"github.com/GenaroNetwork/Genaro/ethdb"
+	"github.com/GenaroNetwork/Genaro/params"
 )
 
 // Genesis block for nodes which don't care about the DAO fork (i.e. not configured)
