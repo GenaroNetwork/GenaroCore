4560f34aeaaa36eeb30dc6f94903a38c2e8ced01
lizhengxiang <yitian@genaro.network>
2018-01-30 20:00:06 +0800

    update

--- mobile/logger.go
+++ mobile/logger.go
@@ -19,7 +19,7 @@ package geth
 import (
 	"os"
 
-	"github.com/GenaroNetwork/GenaroGNX/log"
+	"github.com/GenaroNetwork/Genaro/log"
 )
 
 // SetVerbosity sets the global verbosity level (between 0 and 6 - see logger/verbosity.go).
