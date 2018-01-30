6350816a327aba80203016dc53f46d035ab1b469
lizhengxiang <411566095@qq.com>
2018-01-30 20:00:07 +0800

    update

--- swarm/network/forwarding.go
+++ swarm/network/forwarding.go
@@ -21,8 +21,8 @@ import (
 	"math/rand"
 	"time"
 
-	"github.com/GenaroNetwork/Genaro/log"
-	"github.com/GenaroNetwork/Genaro/swarm/storage"
+	"github.com/GenaroNetwork/GenaroGNX/log"
+	"github.com/GenaroNetwork/GenaroGNX/swarm/storage"
 )
 
 const requesterCount = 3
