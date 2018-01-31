30322dca6c3736be6245dff919c7986a8f08aabe
lizhengxiang <530997877@qq.com>
2018-01-31 20:00:06 +0800

    update

--- cmd/geth/usage.go
+++ cmd/geth/usage.go
@@ -24,8 +24,8 @@ import (
 
 	"strings"
 
-	"github.com/GenaroNetwork/GenaroGNX/cmd/utils"
-	"github.com/GenaroNetwork/GenaroGNX/internal/debug"
+	"github.com/GenaroNetwork/Genaro/cmd/utils"
+	"github.com/GenaroNetwork/Genaro/internal/debug"
 	"gopkg.in/urfave/cli.v1"
 )
 
