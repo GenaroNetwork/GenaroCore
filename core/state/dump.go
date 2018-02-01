b51c0f0f9469171c1ff40ebcb169db41509cf2d8
lizhengxiang <yitian@genaro.network>
2018-02-01 20:00:04 +0800

    update

--- core/state/dump.go
+++ core/state/dump.go
@@ -20,9 +20,9 @@ import (
 	"encoding/json"
 	"fmt"
 
-	"github.com/GenaroNetwork/Genaro/common"
-	"github.com/GenaroNetwork/Genaro/rlp"
-	"github.com/GenaroNetwork/Genaro/trie"
+	"github.com/GenaroNetwork/GenaroGNX/common"
+	"github.com/GenaroNetwork/GenaroGNX/rlp"
+	"github.com/GenaroNetwork/GenaroGNX/trie"
 )
 
 type DumpAccount struct {
