702c7c56d9f59d15231a4da60f4f9f15f2fd907d
lizhengxiang <334506638@qq.com>
2018-02-01 20:00:04 +0800

    update

--- node/node_test.go
+++ node/node_test.go
@@ -24,9 +24,9 @@ import (
 	"testing"
 	"time"
 
-	"github.com/GenaroNetwork/Genaro/crypto"
-	"github.com/GenaroNetwork/Genaro/p2p"
-	"github.com/GenaroNetwork/Genaro/rpc"
+	"github.com/GenaroNetwork/GenaroGNX/crypto"
+	"github.com/GenaroNetwork/GenaroGNX/p2p"
+	"github.com/GenaroNetwork/GenaroGNX/rpc"
 )
 
 var (
