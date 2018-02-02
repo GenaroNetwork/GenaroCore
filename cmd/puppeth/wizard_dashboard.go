007e91fe0a89233fbbcb23aa4802ddaae8d97901
lizhengxiang <yitian@genaro.network>
2018-02-02 20:00:05 +0800

    update

--- cmd/puppeth/wizard_dashboard.go
+++ cmd/puppeth/wizard_dashboard.go
@@ -19,7 +19,7 @@ package main
 import (
 	"fmt"
 
-	"github.com/GenaroNetwork/Genaro/log"
+	"github.com/GenaroNetwork/GenaroGNX/log"
 )
 
 // deployDashboard queries the user for various input on deploying a web-service
