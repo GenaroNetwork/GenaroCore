cf0ca81ae55b8cb4e83bbb5863aab2f7ab488a35
lizhengxiang <334506638@qq.com>
2018-02-02 20:00:06 +0800

    update

--- p2p/discover/table_test.go
+++ p2p/discover/table_test.go
@@ -28,8 +28,8 @@ import (
 	"testing/quick"
 	"time"
 
-	"github.com/GenaroNetwork/Genaro/common"
-	"github.com/GenaroNetwork/Genaro/crypto"
+	"github.com/GenaroNetwork/GenaroGNX/common"
+	"github.com/GenaroNetwork/GenaroGNX/crypto"
 )
 
 func TestTable_pingReplace(t *testing.T) {
