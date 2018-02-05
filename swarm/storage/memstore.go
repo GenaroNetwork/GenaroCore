1561108dd2c816b177fbf5c2ce92e98917678601
lizhengxiang <411566095@qq.com>
2018-02-05 20:00:06 +0800

    update

--- swarm/storage/memstore.go
+++ swarm/storage/memstore.go
@@ -22,8 +22,8 @@ import (
 	"fmt"
 	"sync"
 
-	"github.com/GenaroNetwork/Genaro/log"
-	"github.com/GenaroNetwork/Genaro/metrics"
+	"github.com/GenaroNetwork/GenaroGNX/log"
+	"github.com/GenaroNetwork/GenaroGNX/metrics"
 )
 
 //metrics variables
