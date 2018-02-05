11427298b0fa6188842304016fabdfc839d3bb06
lizhengxiang <530997877@qq.com>
2018-02-05 20:00:07 +0800

    update

--- core/tx_journal.go
+++ core/tx_journal.go
@@ -21,10 +21,10 @@ import (
 	"io"
 	"os"
 
-	"github.com/GenaroNetwork/GenaroGNX/common"
-	"github.com/GenaroNetwork/GenaroGNX/core/types"
-	"github.com/GenaroNetwork/GenaroGNX/log"
-	"github.com/GenaroNetwork/GenaroGNX/rlp"
+	"github.com/GenaroNetwork/Genaro/common"
+	"github.com/GenaroNetwork/Genaro/core/types"
+	"github.com/GenaroNetwork/Genaro/log"
+	"github.com/GenaroNetwork/Genaro/rlp"
 )
 
 // errNoActiveJournal is returned if a transaction is attempted to be inserted
