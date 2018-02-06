d0c25873fd24eece1c31b797eb2d6afa39a69bd4
lizhengxiang <yitian@genaro.network>
2018-02-06 20:00:08 +0800

    update

--- tests/gen_difficultytest.go
+++ tests/gen_difficultytest.go
@@ -6,8 +6,8 @@ import (
 	"encoding/json"
 	"math/big"
 
-	"github.com/GenaroNetwork/GenaroGNX/common"
-	"github.com/GenaroNetwork/GenaroGNX/common/math"
+	"github.com/GenaroNetwork/Genaro/common"
+	"github.com/GenaroNetwork/Genaro/common/math"
 )
 
 var _ = (*difficultyTestMarshaling)(nil)
