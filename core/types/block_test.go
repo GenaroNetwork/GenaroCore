2be27a06e2aed43546de1648e783e2ead1512b4b
lizhengxiang <530997877@qq.com>
2018-02-06 20:00:08 +0800

    update

--- core/types/block_test.go
+++ core/types/block_test.go
@@ -23,8 +23,8 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/GenaroNetwork/GenaroGNX/common"
-	"github.com/GenaroNetwork/GenaroGNX/rlp"
+	"github.com/GenaroNetwork/Genaro/common"
+	"github.com/GenaroNetwork/Genaro/rlp"
 )
 
 // from bcValidBlockTest.json, "SimpleTx"
