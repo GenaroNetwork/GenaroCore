436d13c57194097da878a7ca5044f68346533e0b
lizhengxiang <334506638@qq.com>
2018-02-06 20:00:08 +0800

    update

--- event/example_scope_test.go
+++ event/example_scope_test.go
@@ -20,7 +20,7 @@ import (
 	"fmt"
 	"sync"
 
-	"github.com/GenaroNetwork/Genaro/event"
+	"github.com/GenaroNetwork/GenaroGNX/event"
 )
 
 // This example demonstrates how SubscriptionScope can be used to control the lifetime of
