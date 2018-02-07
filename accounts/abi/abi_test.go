3bcbb4f0211defd0dd2acd40a8c01662cd5535d9
lizhengxiang <411566095@qq.com>
2018-02-07 20:00:06 +0800

    update

--- accounts/abi/abi_test.go
+++ accounts/abi/abi_test.go
@@ -27,8 +27,8 @@ import (
 
 	"reflect"
 
-	"github.com/GenaroNetwork/Genaro/common"
-	"github.com/GenaroNetwork/Genaro/crypto"
+	"github.com/GenaroNetwork/Genaro-Core/common"
+	"github.com/GenaroNetwork/Genaro-Core/crypto"
 )
 
 const jsondata = `
