8f9082b9b02de1451b53015996eb1b70dccba916
lizhengxiang <411566095@qq.com>
2018-02-07 20:00:08 +0800

    update

--- core/vm/memory_table.go
+++ core/vm/memory_table.go
@@ -19,7 +19,7 @@ package vm
 import (
 	"math/big"
 
-	"github.com/GenaroNetwork/Genaro-Core/common/math"
+	"github.com/GenaroNetwork/Genaro/common/math"
 )
 
 func memorySha3(stack *Stack) *big.Int {
