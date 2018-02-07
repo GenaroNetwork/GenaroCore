7e537e3bae7e2af3d193f8e452269673d2efb4c5
lizhengxiang <411566095@qq.com>
2018-02-07 20:00:07 +0800

    update

--- common/number/int.go
+++ common/number/int.go
@@ -19,7 +19,7 @@ package number
 import (
 	"math/big"
 
-	"github.com/GenaroNetwork/Genaro-Core/common"
+	"github.com/GenaroNetwork/Genaro/common"
 )
 
 var tt256 = new(big.Int).Lsh(big.NewInt(1), 256)
