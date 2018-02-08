d7af7efdf165d48f90cadb5eb327ca2d9e714ccc
lizhengxiang <530997877@qq.com>
2018-02-08 20:00:08 +0800

    update

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -11,7 +11,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/GenaroNetwork/Genaro/log"
+	"github.com/GenaroNetwork/Genaro-Core/log"
 )
 
 // Enabled is checked by the constructor functions for all of the
