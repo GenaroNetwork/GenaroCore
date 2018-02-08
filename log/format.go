21cbbe9902243b50c2a158b672575a7f958d1b82
lizhengxiang <530997877@qq.com>
2018-02-08 20:00:06 +0800

    update

--- log/format.go
+++ log/format.go
@@ -22,7 +22,7 @@ const (
 
 // locationTrims are trimmed for display to avoid unwieldy log lines.
 var locationTrims = []string{
-	"github.com/GenaroNetwork/Genaro/",
+	"github.com/GenaroNetwork/Genaro-Core/",
 }
 
 // PrintOrigins sets or unsets log location (file:line) printing for terminal
