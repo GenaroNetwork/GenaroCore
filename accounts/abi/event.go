1402e10f896a94def595f0f758569802004be764
lizhengxiang <yitian@genaro.network>
2018-02-09 20:00:05 +0800

    update

--- accounts/abi/event.go
+++ accounts/abi/event.go
@@ -20,8 +20,8 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/GenaroNetwork/Genaro-Core/common"
-	"github.com/GenaroNetwork/Genaro-Core/crypto"
+	"github.com/GenaroNetwork/Genaro/common"
+	"github.com/GenaroNetwork/Genaro/crypto"
 )
 
 // Event is an event potentially triggered by the EVM's LOG mechanism. The Event
