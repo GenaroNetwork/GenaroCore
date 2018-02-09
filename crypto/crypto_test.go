d7ce7043493d1df059e956d0e520ed749d7052db
lizhengxiang <411566095@qq.com>
2018-02-09 20:00:07 +0800

    update

--- crypto/crypto_test.go
+++ crypto/crypto_test.go
@@ -25,7 +25,7 @@ import (
 	"os"
 	"testing"
 
-	"github.com/GenaroNetwork/Genaro-Core/common"
+	"github.com/GenaroNetwork/Genaro/common"
 )
 
 var testAddrHex = "970e8128ab834e8eac17ab8e3812f010678cf791"
