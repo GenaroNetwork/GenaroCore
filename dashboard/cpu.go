308563a11d6b84b147322777a8c2674b120ee470
lizhengxiang <411566095@qq.com>
2018-02-26 21:14:34 +0800

    update

--- dashboard/cpu.go
+++ dashboard/cpu.go
@@ -21,7 +21,7 @@ package dashboard
 import (
 	"syscall"
 
-	"github.com/GenaroNetwork/Genaro-Core/log"
+	"github.com/GenaroNetwork/Genaro/log"
 )
 
 // getProcessCPUTime retrieves the process' CPU time since program startup.
