b0c5dc35aab2beba783c3120a94cdd2fdd5fd1ac
lizhengxiang <yitian@genaro.network>
2018-02-27 21:14:33 +0800

    update

--- accounts/keystore/keystore_plain_test.go
+++ accounts/keystore/keystore_plain_test.go
@@ -27,8 +27,8 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/GenaroNetwork/Genaro-Core/common"
-	"github.com/GenaroNetwork/Genaro-Core/crypto"
+	"github.com/GenaroNetwork/Genaro/common"
+	"github.com/GenaroNetwork/Genaro/crypto"
 )
 
 func tmpKeyStoreIface(t *testing.T, encrypted bool) (dir string, ks keyStore) {
