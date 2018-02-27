35fc4e8ec67316431adb500435d8a729ef70d5fb
lizhengxiang <sophia@genaro.network>
2018-02-27 21:14:34 +0800

    update

--- mobile/params.go
+++ mobile/params.go
@@ -21,9 +21,9 @@ package geth
 import (
 	"encoding/json"
 
-	"github.com/GenaroNetwork/Genaro-Core/core"
-	"github.com/GenaroNetwork/Genaro-Core/p2p/discv5"
-	"github.com/GenaroNetwork/Genaro-Core/params"
+	"github.com/GenaroNetwork/Genaro/core"
+	"github.com/GenaroNetwork/Genaro/p2p/discv5"
+	"github.com/GenaroNetwork/Genaro/params"
 )
 
 // MainnetGenesis returns the JSON spec to use for the main Ethereum network. It
