e603cc69c86cfd14c0bd78889c58f30fddbe1666
lizhengxiang <sophia@genaro.network>
2018-02-28 21:14:32 +0800

    update

--- contracts/ens/ens.go
+++ contracts/ens/ens.go
@@ -23,11 +23,11 @@ package ens
 import (
 	"strings"
 
-	"github.com/GenaroNetwork/Genaro/accounts/abi/bind"
-	"github.com/GenaroNetwork/Genaro/common"
-	"github.com/GenaroNetwork/Genaro/contracts/ens/contract"
-	"github.com/GenaroNetwork/Genaro/core/types"
-	"github.com/GenaroNetwork/Genaro/crypto"
+	"github.com/GenaroNetwork/Genaro-Core/accounts/abi/bind"
+	"github.com/GenaroNetwork/Genaro-Core/common"
+	"github.com/GenaroNetwork/Genaro-Core/contracts/ens/contract"
+	"github.com/GenaroNetwork/Genaro-Core/core/types"
+	"github.com/GenaroNetwork/Genaro-Core/crypto"
 )
 
 var (
