7eef918b1c55aaf5b93fed1827791b2593581ed7
lizhengxiang <411566095@qq.com>
2018-02-28 21:14:32 +0800

    update

--- accounts/usbwallet/ledger.go
+++ accounts/usbwallet/ledger.go
@@ -28,12 +28,12 @@ import (
 	"io"
 	"math/big"
 
-	"github.com/GenaroNetwork/Genaro/accounts"
-	"github.com/GenaroNetwork/Genaro/common"
-	"github.com/GenaroNetwork/Genaro/common/hexutil"
-	"github.com/GenaroNetwork/Genaro/core/types"
-	"github.com/GenaroNetwork/Genaro/log"
-	"github.com/GenaroNetwork/Genaro/rlp"
+	"github.com/GenaroNetwork/Genaro-Core/accounts"
+	"github.com/GenaroNetwork/Genaro-Core/common"
+	"github.com/GenaroNetwork/Genaro-Core/common/hexutil"
+	"github.com/GenaroNetwork/Genaro-Core/core/types"
+	"github.com/GenaroNetwork/Genaro-Core/log"
+	"github.com/GenaroNetwork/Genaro-Core/rlp"
 )
 
 // ledgerOpcode is an enumeration encoding the supported Ledger opcodes.
