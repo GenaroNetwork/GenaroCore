d452c2b25d560eae54848eb2ea1e797509f24be8
lizhengxiang <411566095@qq.com>
2018-02-28 21:14:33 +0800

    update

--- contracts/ens/ens_test.go
+++ contracts/ens/ens_test.go
@@ -20,11 +20,11 @@ import (
 	"math/big"
 	"testing"
 
-	"github.com/GenaroNetwork/Genaro-Core/accounts/abi/bind"
-	"github.com/GenaroNetwork/Genaro-Core/accounts/abi/bind/backends"
-	"github.com/GenaroNetwork/Genaro-Core/contracts/ens/contract"
-	"github.com/GenaroNetwork/Genaro-Core/core"
-	"github.com/GenaroNetwork/Genaro-Core/crypto"
+	"github.com/GenaroNetwork/Genaro/accounts/abi/bind"
+	"github.com/GenaroNetwork/Genaro/accounts/abi/bind/backends"
+	"github.com/GenaroNetwork/Genaro/contracts/ens/contract"
+	"github.com/GenaroNetwork/Genaro/core"
+	"github.com/GenaroNetwork/Genaro/crypto"
 )
 
 var (
