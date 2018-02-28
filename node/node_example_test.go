d282de388217d2c738a9191118928830a9017c73
lizhengxiang <sophia@genaro.network>
2018-02-28 21:14:34 +0800

    update

--- node/node_example_test.go
+++ node/node_example_test.go
@@ -20,9 +20,9 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/GenaroNetwork/Genaro-Core/node"
-	"github.com/GenaroNetwork/Genaro-Core/p2p"
-	"github.com/GenaroNetwork/Genaro-Core/rpc"
+	"github.com/GenaroNetwork/Genaro/node"
+	"github.com/GenaroNetwork/Genaro/p2p"
+	"github.com/GenaroNetwork/Genaro/rpc"
 )
 
 // SampleService is a trivial network service that can be attached to a node for
