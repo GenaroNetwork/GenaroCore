846d3400adc51391becdf0506b28db4b56a1e444
lizhengxiang <sophia@genaro.network>
2018-03-01 21:14:34 +0800

    update

--- eth/filters/api_test.go
+++ eth/filters/api_test.go
@@ -21,8 +21,8 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/GenaroNetwork/Genaro/common"
-	"github.com/GenaroNetwork/Genaro/rpc"
+	"github.com/GenaroNetwork/Genaro-Core/common"
+	"github.com/GenaroNetwork/Genaro-Core/rpc"
 )
 
 func TestUnmarshalJSONNewFilterArgs(t *testing.T) {
