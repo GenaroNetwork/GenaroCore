bdf279dfabe554cffd9ea3b194f9478413fb5f78
lizhengxiang <yitian@genaro.network>
2018-03-01 21:14:34 +0800

    update

--- cmd/abigen/main.go
+++ cmd/abigen/main.go
@@ -24,8 +24,8 @@ import (
 	"os"
 	"strings"
 
-	"github.com/GenaroNetwork/Genaro-Core/accounts/abi/bind"
-	"github.com/GenaroNetwork/Genaro-Core/common/compiler"
+	"github.com/GenaroNetwork/Genaro/accounts/abi/bind"
+	"github.com/GenaroNetwork/Genaro/common/compiler"
 )
 
 var (
