0fc67a8be7bfdcfeaefc10f3349af955ff87a9d9
lizhengxiang <sophia@genaro.network>
2018-03-01 21:14:32 +0800

    update

--- eth/fetcher/fetcher.go
+++ eth/fetcher/fetcher.go
@@ -22,10 +22,10 @@ import (
 	"math/rand"
 	"time"
 
-	"github.com/GenaroNetwork/Genaro/common"
-	"github.com/GenaroNetwork/Genaro/consensus"
-	"github.com/GenaroNetwork/Genaro/core/types"
-	"github.com/GenaroNetwork/Genaro/log"
+	"github.com/GenaroNetwork/Genaro-Core/common"
+	"github.com/GenaroNetwork/Genaro-Core/consensus"
+	"github.com/GenaroNetwork/Genaro-Core/core/types"
+	"github.com/GenaroNetwork/Genaro-Core/log"
 	"gopkg.in/karalabe/cookiejar.v2/collections/prque"
 )
 
