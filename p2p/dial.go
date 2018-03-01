533c1b2e2e5443795a0428a5c6553de59991d9c4
lizhengxiang <sophia@genaro.network>
2018-03-01 21:14:32 +0800

    update

--- p2p/dial.go
+++ p2p/dial.go
@@ -24,9 +24,9 @@ import (
 	"net"
 	"time"
 
-	"github.com/GenaroNetwork/Genaro-Core/log"
-	"github.com/GenaroNetwork/Genaro-Core/p2p/discover"
-	"github.com/GenaroNetwork/Genaro-Core/p2p/netutil"
+	"github.com/GenaroNetwork/Genaro/log"
+	"github.com/GenaroNetwork/Genaro/p2p/discover"
+	"github.com/GenaroNetwork/Genaro/p2p/netutil"
 )
 
 const (
