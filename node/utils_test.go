ee3053ab5a6f53778739ba265cfe2413689696f0
lizhengxiang <sophia@genaro.network>
2018-03-01 21:14:32 +0800

    update

--- node/utils_test.go
+++ node/utils_test.go
@@ -22,8 +22,8 @@ package node
 import (
 	"reflect"
 
-	"github.com/GenaroNetwork/Genaro-Core/p2p"
-	"github.com/GenaroNetwork/Genaro-Core/rpc"
+	"github.com/GenaroNetwork/Genaro/p2p"
+	"github.com/GenaroNetwork/Genaro/rpc"
 )
 
 // NoopService is a trivial implementation of the Service interface.
