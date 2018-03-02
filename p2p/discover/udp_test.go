a97582c1546265300fd604e10da8bceae5b8ec9f
lizhengxiang <530997877@qq.com>
2018-03-02 21:14:33 +0800

    update

--- p2p/discover/udp_test.go
+++ p2p/discover/udp_test.go
@@ -34,9 +34,9 @@ import (
 	"time"
 
 	"github.com/davecgh/go-spew/spew"
-	"github.com/GenaroNetwork/Genaro/common"
-	"github.com/GenaroNetwork/Genaro/crypto"
-	"github.com/GenaroNetwork/Genaro/rlp"
+	"github.com/GenaroNetwork/Genaro-Core/common"
+	"github.com/GenaroNetwork/Genaro-Core/crypto"
+	"github.com/GenaroNetwork/Genaro-Core/rlp"
 )
 
 func init() {
