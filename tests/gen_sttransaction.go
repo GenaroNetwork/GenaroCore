baf40ccbff294f9da772cb56fd97d21dc6f0457c
lizhengxiang <sophia.littlefox@gmail.com>
2018-03-02 21:14:35 +0800

    update

--- tests/gen_sttransaction.go
+++ tests/gen_sttransaction.go
@@ -6,8 +6,8 @@ import (
 	"encoding/json"
 	"math/big"
 
-	"github.com/GenaroNetwork/Genaro/common/hexutil"
-	"github.com/GenaroNetwork/Genaro/common/math"
+	"github.com/GenaroNetwork/Genaro-Core/common/hexutil"
+	"github.com/GenaroNetwork/Genaro-Core/common/math"
 )
 
 var _ = (*stTransactionMarshaling)(nil)
