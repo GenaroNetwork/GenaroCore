b6df470266d989f46c0dc8c805153b96f4cc0692
lizhengxiang <sophia@genaro.network>
2018-03-02 21:14:33 +0800

    update

--- p2p/discover/udp.go
+++ p2p/discover/udp.go
@@ -25,11 +25,11 @@ import (
 	"net"
 	"time"
 
-	"github.com/GenaroNetwork/Genaro-Core/crypto"
-	"github.com/GenaroNetwork/Genaro-Core/log"
-	"github.com/GenaroNetwork/Genaro-Core/p2p/nat"
-	"github.com/GenaroNetwork/Genaro-Core/p2p/netutil"
-	"github.com/GenaroNetwork/Genaro-Core/rlp"
+	"github.com/GenaroNetwork/Genaro/crypto"
+	"github.com/GenaroNetwork/Genaro/log"
+	"github.com/GenaroNetwork/Genaro/p2p/nat"
+	"github.com/GenaroNetwork/Genaro/p2p/netutil"
+	"github.com/GenaroNetwork/Genaro/rlp"
 )
 
 const Version = 4
