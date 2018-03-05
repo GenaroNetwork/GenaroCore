0060989f4ec85a652f22d91091537a4c581cdd85
lizhengxiang <411566095@qq.com>
2018-03-05 21:14:33 +0800

    update

--- core/gen_genesis.go
+++ core/gen_genesis.go
@@ -7,10 +7,10 @@ import (
 	"errors"
 	"math/big"
 
-	"github.com/GenaroNetwork/Genaro-Core/common"
-	"github.com/GenaroNetwork/Genaro-Core/common/hexutil"
-	"github.com/GenaroNetwork/Genaro-Core/common/math"
-	"github.com/GenaroNetwork/Genaro-Core/params"
+	"github.com/GenaroNetwork/Genaro/common"
+	"github.com/GenaroNetwork/Genaro/common/hexutil"
+	"github.com/GenaroNetwork/Genaro/common/math"
+	"github.com/GenaroNetwork/Genaro/params"
 )
 
 var _ = (*genesisSpecMarshaling)(nil)
