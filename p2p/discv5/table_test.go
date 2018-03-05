c4146b8d34d89f6c96d9be29ffbd0f8ce876ce83
lizhengxiang <sophia@genaro.network>
2018-03-05 21:14:34 +0800

    update

--- p2p/discv5/table_test.go
+++ p2p/discv5/table_test.go
@@ -27,8 +27,8 @@ import (
 	"testing/quick"
 	"time"
 
-	"github.com/GenaroNetwork/Genaro-Core/common"
-	"github.com/GenaroNetwork/Genaro-Core/crypto"
+	"github.com/GenaroNetwork/Genaro/common"
+	"github.com/GenaroNetwork/Genaro/crypto"
 )
 
 type nullTransport struct{}
