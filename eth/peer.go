575d315c83bdd4f4ff92851c1bd1a01ec8aa98c0
lizhengxiang <411566095@qq.com>
2018-03-05 21:14:33 +0800

    update

--- eth/peer.go
+++ eth/peer.go
@@ -23,10 +23,10 @@ import (
 	"sync"
 	"time"
 
-	"github.com/GenaroNetwork/Genaro-Core/common"
-	"github.com/GenaroNetwork/Genaro-Core/core/types"
-	"github.com/GenaroNetwork/Genaro-Core/p2p"
-	"github.com/GenaroNetwork/Genaro-Core/rlp"
+	"github.com/GenaroNetwork/Genaro/common"
+	"github.com/GenaroNetwork/Genaro/core/types"
+	"github.com/GenaroNetwork/Genaro/p2p"
+	"github.com/GenaroNetwork/Genaro/rlp"
 	"gopkg.in/fatih/set.v0"
 )
 
