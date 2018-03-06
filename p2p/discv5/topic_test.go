f8a71dfb7b549d1a2b541e69b1728e64a363a032
lizhengxiang <sophia@genaro.network>
2018-03-06 21:14:32 +0800

    update

--- p2p/discv5/topic_test.go
+++ p2p/discv5/topic_test.go
@@ -21,8 +21,8 @@ import (
 	"testing"
 	"time"
 
-	"github.com/GenaroNetwork/Genaro-Core/common"
-	"github.com/GenaroNetwork/Genaro-Core/common/mclock"
+	"github.com/GenaroNetwork/Genaro/common"
+	"github.com/GenaroNetwork/Genaro/common/mclock"
 )
 
 func TestTopicRadius(t *testing.T) {
