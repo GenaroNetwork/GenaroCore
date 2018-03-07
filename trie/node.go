c33dbab08f9c44d5024702439ad34ced4c13a364
lizhengxiang <530997877@qq.com>
2018-03-07 21:14:33 +0800

    update

--- trie/node.go
+++ trie/node.go
@@ -21,8 +21,8 @@ import (
 	"io"
 	"strings"
 
-	"github.com/GenaroNetwork/Genaro-Core/common"
-	"github.com/GenaroNetwork/Genaro-Core/rlp"
+	"github.com/GenaroNetwork/Genaro/common"
+	"github.com/GenaroNetwork/Genaro/rlp"
 )
 
 var indices = []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "[17]"}
