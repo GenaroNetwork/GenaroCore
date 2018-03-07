2bae8a4fb6cc2cacec2225e21ad6c1856add06b6
lizhengxiang <411566095@qq.com>
2018-03-07 21:14:34 +0800

    update

--- cmd/geth/monitorcmd.go
+++ cmd/geth/monitorcmd.go
@@ -25,9 +25,9 @@ import (
 	"strings"
 	"time"
 
-	"github.com/GenaroNetwork/Genaro/cmd/utils"
-	"github.com/GenaroNetwork/Genaro/node"
-	"github.com/GenaroNetwork/Genaro/rpc"
+	"github.com/GenaroNetwork/Genaro-Core/cmd/utils"
+	"github.com/GenaroNetwork/Genaro-Core/node"
+	"github.com/GenaroNetwork/Genaro-Core/rpc"
 	"github.com/gizak/termui"
 	"gopkg.in/urfave/cli.v1"
 )
