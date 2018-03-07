58f5426c110cb3e3b19ef826999319c550c9a4b0
lizhengxiang <530997877@qq.com>
2018-03-07 21:14:31 +0800

    update

--- trie/errors.go
+++ trie/errors.go
@@ -19,7 +19,7 @@ package trie
 import (
 	"fmt"
 
-	"github.com/GenaroNetwork/Genaro-Core/common"
+	"github.com/GenaroNetwork/Genaro/common"
 )
 
 // MissingNodeError is returned by the trie functions (TryGet, TryUpdate, TryDelete)
