f7878818e1bd105fd4811eccfbc74925f2e016c0
lizhengxiang <530997877@qq.com>
2018-03-08 21:14:33 +0800

    update

--- p2p/protocols/protocol_test.go
+++ p2p/protocols/protocol_test.go
@@ -23,10 +23,10 @@ import (
 	"testing"
 	"time"
 
-	"github.com/GenaroNetwork/Genaro/p2p"
-	"github.com/GenaroNetwork/Genaro/p2p/discover"
-	"github.com/GenaroNetwork/Genaro/p2p/simulations/adapters"
-	p2ptest "github.com/GenaroNetwork/Genaro/p2p/testing"
+	"github.com/GenaroNetwork/Genaro-Core/p2p"
+	"github.com/GenaroNetwork/Genaro-Core/p2p/discover"
+	"github.com/GenaroNetwork/Genaro-Core/p2p/simulations/adapters"
+	p2ptest "github.com/GenaroNetwork/Genaro-Core/p2p/testing"
 )
 
 // handshake message type
