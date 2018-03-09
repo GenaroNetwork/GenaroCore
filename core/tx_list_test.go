43f14bbca2e54562573d9535ce1da06050344ff0
lizhengxiang <411566095@qq.com>
2018-03-09 21:14:33 +0800

    update

--- core/tx_list_test.go
+++ core/tx_list_test.go
@@ -20,8 +20,8 @@ import (
 	"math/rand"
 	"testing"
 
-	"github.com/GenaroNetwork/Genaro-Core/core/types"
-	"github.com/GenaroNetwork/Genaro-Core/crypto"
+	"github.com/GenaroNetwork/Genaro/core/types"
+	"github.com/GenaroNetwork/Genaro/crypto"
 )
 
 // Tests that transactions can be added to strict lists and list contents and
