e5623140cd6e97425522092847a89e3a3594b8a2
lizhengxiang <530997877@qq.com>
2018-03-09 21:14:35 +0800

    update

--- whisper/whisperv5/benchmarks_test.go
+++ whisper/whisperv5/benchmarks_test.go
@@ -19,7 +19,7 @@ package whisperv5
 import (
 	"testing"
 
-	"github.com/GenaroNetwork/Genaro-Core/crypto"
+	"github.com/GenaroNetwork/Genaro/crypto"
 )
 
 func BenchmarkDeriveKeyMaterial(b *testing.B) {
