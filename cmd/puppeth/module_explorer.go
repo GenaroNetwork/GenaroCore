1c8b8026d6b0d0516ac1ac84b958ea199dda9e8b
lizhengxiang <411566095@qq.com>
2018-03-09 21:14:32 +0800

    update

--- cmd/puppeth/module_explorer.go
+++ cmd/puppeth/module_explorer.go
@@ -25,7 +25,7 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/GenaroNetwork/Genaro/log"
+	"github.com/GenaroNetwork/Genaro-Core/log"
 )
 
 // explorerDockerfile is the Dockerfile required to run a block explorer.
