345de01f9940b69ef5782f0fac3c606a78cfa285
lizhengxiang <sophia@genaro.network>
2018-03-09 21:14:33 +0800

    update

--- core/tx_list.go
+++ core/tx_list.go
@@ -22,9 +22,9 @@ import (
 	"math/big"
 	"sort"
 
-	"github.com/GenaroNetwork/Genaro/common"
-	"github.com/GenaroNetwork/Genaro/core/types"
-	"github.com/GenaroNetwork/Genaro/log"
+	"github.com/GenaroNetwork/Genaro-Core/common"
+	"github.com/GenaroNetwork/Genaro-Core/core/types"
+	"github.com/GenaroNetwork/Genaro-Core/log"
 )
 
 // nonceHeap is a heap.Interface implementation over 64bit unsigned integers for
