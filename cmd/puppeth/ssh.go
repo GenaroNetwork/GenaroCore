e8813339c21d0e21cb854d0c0b69fc898f837195
lizhengxiang <411566095@qq.com>
2018-03-12 20:14:17 +0800

    update

--- cmd/puppeth/ssh.go
+++ cmd/puppeth/ssh.go
@@ -28,7 +28,7 @@ import (
 	"path/filepath"
 	"strings"
 
-	"github.com/GenaroNetwork/Genaro/log"
+	"github.com/GenaroNetwork/Genaro-Core/log"
 	"golang.org/x/crypto/ssh"
 	"golang.org/x/crypto/ssh/terminal"
 )
