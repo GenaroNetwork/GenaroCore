2877b8cf1eac04cd9f95d9fb6b957e3cc985799b
lizhengxiang <411566095@qq.com>
2018-03-12 20:14:19 +0800

    update

--- cmd/puppeth/wizard_explorer.go
+++ cmd/puppeth/wizard_explorer.go
@@ -21,7 +21,7 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/GenaroNetwork/Genaro/log"
+	"github.com/GenaroNetwork/Genaro-Core/log"
 )
 
 // deployExplorer creates a new block explorer based on some user input.
