822073f808bb292176ef912eea0584ea1431c1de
lizhengxiang <530997877@qq.com>
2018-03-13 20:14:18 +0800

    update

--- whisper/whisperv5/message_test.go
+++ whisper/whisperv5/message_test.go
@@ -21,8 +21,8 @@ import (
 	mrand "math/rand"
 	"testing"
 
-	"github.com/GenaroNetwork/Genaro-Core/crypto"
-	"github.com/GenaroNetwork/Genaro-Core/rlp"
+	"github.com/GenaroNetwork/Genaro/crypto"
+	"github.com/GenaroNetwork/Genaro/rlp"
 )
 
 func generateMessageParams() (*MessageParams, error) {
