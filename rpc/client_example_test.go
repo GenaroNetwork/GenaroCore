098cc8b3e99bb917245bda4e432fa4c991613a30
lizhengxiang <sophia@genaro.network>
2018-03-14 20:14:18 +0800

    update

--- rpc/client_example_test.go
+++ rpc/client_example_test.go
@@ -22,7 +22,7 @@ import (
 	"math/big"
 	"time"
 
-	"github.com/GenaroNetwork/Genaro-Core/rpc"
+	"github.com/GenaroNetwork/Genaro/rpc"
 )
 
 // In this example, our client whishes to track the latest 'block number'
