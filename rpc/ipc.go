895ada50921da6f7002312915eb657ecd77dcb50
lizhengxiang <sophia@genaro.network>
2018-03-14 20:14:19 +0800

    update

--- rpc/ipc.go
+++ rpc/ipc.go
@@ -21,7 +21,7 @@ import (
 	"fmt"
 	"net"
 
-	"github.com/GenaroNetwork/Genaro-Core/log"
+	"github.com/GenaroNetwork/Genaro/log"
 )
 
 // CreateIPCListener creates an listener, on Unix platforms this is a unix socket, on
