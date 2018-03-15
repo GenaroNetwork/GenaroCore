9998f2b7fcb57f3ab8c818d2b260faac1ff0a21d
lizhengxiang <530997877@qq.com>
2018-03-15 20:14:17 +0800

    update

--- whisper/whisperv6/envelope_test.go
+++ whisper/whisperv6/envelope_test.go
@@ -22,7 +22,7 @@ import (
 	mrand "math/rand"
 	"testing"
 
-	"github.com/GenaroNetwork/Genaro-Core/crypto"
+	"github.com/GenaroNetwork/Genaro/crypto"
 )
 
 func TestEnvelopeOpenAcceptsOnlyOneKeyTypeInFilter(t *testing.T) {
