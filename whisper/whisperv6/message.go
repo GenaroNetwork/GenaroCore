d1fad58b808557c0a03fccb82df6c710551d53fc
lizhengxiang <530997877@qq.com>
2018-03-16 20:14:17 +0800

    update

--- whisper/whisperv6/message.go
+++ whisper/whisperv6/message.go
@@ -28,10 +28,10 @@ import (
 	mrand "math/rand"
 	"strconv"
 
-	"github.com/GenaroNetwork/Genaro-Core/common"
-	"github.com/GenaroNetwork/Genaro-Core/crypto"
-	"github.com/GenaroNetwork/Genaro-Core/crypto/ecies"
-	"github.com/GenaroNetwork/Genaro-Core/log"
+	"github.com/GenaroNetwork/Genaro/common"
+	"github.com/GenaroNetwork/Genaro/crypto"
+	"github.com/GenaroNetwork/Genaro/crypto/ecies"
+	"github.com/GenaroNetwork/Genaro/log"
 )
 
 // MessageParams specifies the exact way a message should be wrapped
