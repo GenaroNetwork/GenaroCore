035fa328ec73845a0c5344dcf9d63a4bea0e27c0
lizhengxiang <530997877@qq.com>
2018-03-16 20:14:18 +0800

    update

--- whisper/whisperv6/peer.go
+++ whisper/whisperv6/peer.go
@@ -22,10 +22,10 @@ import (
 	"sync"
 	"time"
 
-	"github.com/GenaroNetwork/Genaro-Core/common"
-	"github.com/GenaroNetwork/Genaro-Core/log"
-	"github.com/GenaroNetwork/Genaro-Core/p2p"
-	"github.com/GenaroNetwork/Genaro-Core/rlp"
+	"github.com/GenaroNetwork/Genaro/common"
+	"github.com/GenaroNetwork/Genaro/log"
+	"github.com/GenaroNetwork/Genaro/p2p"
+	"github.com/GenaroNetwork/Genaro/rlp"
 	set "gopkg.in/fatih/set.v0"
 )
 
