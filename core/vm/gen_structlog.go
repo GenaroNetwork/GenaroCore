e11b6fee2fcf589874ff4a451347b29fc2fc61bc
lizhengxiang <sophia@genaro.network>
2018-03-16 20:14:19 +0800

    update

--- core/vm/gen_structlog.go
+++ core/vm/gen_structlog.go
@@ -6,9 +6,9 @@ import (
 	"encoding/json"
 	"math/big"
 
-	"github.com/GenaroNetwork/Genaro/common"
-	"github.com/GenaroNetwork/Genaro/common/hexutil"
-	"github.com/GenaroNetwork/Genaro/common/math"
+	"github.com/GenaroNetwork/Genaro-Core/common"
+	"github.com/GenaroNetwork/Genaro-Core/common/hexutil"
+	"github.com/GenaroNetwork/Genaro-Core/common/math"
 )
 
 var _ = (*structLogMarshaling)(nil)
