14f8ef701a95e135086804cd7e335a93ff94b21f
lizhengxiang <sophia@genaro.network>
2018-03-23 22:14:29 +0800

    update

--- core/vm/analysis.go
+++ core/vm/analysis.go
@@ -19,7 +19,7 @@ package vm
 import (
 	"math/big"
 
-	"github.com/GenaroNetwork/Genaro/common"
+	"github.com/GenaroNetwork/Genaro-Core/common"
 )
 
 // destinations stores one map per contract (keyed by hash of code).
