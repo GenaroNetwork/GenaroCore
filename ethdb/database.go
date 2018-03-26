0e2cd6ec234156fceed9a642a45f2dc67d647326
lizhengxiang <411566095@qq.com>
2018-03-26 23:14:27 +0800

    update

--- ethdb/database.go
+++ ethdb/database.go
@@ -22,8 +22,8 @@ import (
 	"sync"
 	"time"
 
-	"github.com/GenaroNetwork/Genaro/log"
-	"github.com/GenaroNetwork/Genaro/metrics"
+	"github.com/GenaroNetwork/Genaro-Core/log"
+	"github.com/GenaroNetwork/Genaro-Core/metrics"
 	"github.com/syndtr/goleveldb/leveldb"
 	"github.com/syndtr/goleveldb/leveldb/errors"
 	"github.com/syndtr/goleveldb/leveldb/filter"
