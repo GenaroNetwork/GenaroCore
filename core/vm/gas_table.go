434c2f4170579c9aee84f2a026031c93c33477c0
lizhengxiang <sophia@genaro.network>
2018-03-27 23:14:26 +0800

    update

--- core/vm/gas_table.go
+++ core/vm/gas_table.go
@@ -17,9 +17,9 @@
 package vm
 
 import (
-	"github.com/GenaroNetwork/Genaro/common"
-	"github.com/GenaroNetwork/Genaro/common/math"
-	"github.com/GenaroNetwork/Genaro/params"
+	"github.com/GenaroNetwork/Genaro-Core/common"
+	"github.com/GenaroNetwork/Genaro-Core/common/math"
+	"github.com/GenaroNetwork/Genaro-Core/params"
 )
 
 // memoryGasCosts calculates the quadratic gas for memory expansion. It does so
