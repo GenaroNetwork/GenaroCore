90a2cc987f47c6abf41bb1e2fd32da425171b47a
lizhengxiang <sophia@genaro.network>
2018-04-11 01:14:21 +0800

    update

--- cmd/puppeth/genesis.go
+++ cmd/puppeth/genesis.go
@@ -21,11 +21,11 @@ import (
 	"errors"
 	"math"
 
-	"github.com/GenaroNetwork/Genaro/common"
-	"github.com/GenaroNetwork/Genaro/common/hexutil"
-	"github.com/GenaroNetwork/Genaro/consensus/ethash"
-	"github.com/GenaroNetwork/Genaro/core"
-	"github.com/GenaroNetwork/Genaro/params"
+	"github.com/GenaroNetwork/Genaro-Core/common"
+	"github.com/GenaroNetwork/Genaro-Core/common/hexutil"
+	"github.com/GenaroNetwork/Genaro-Core/consensus/ethash"
+	"github.com/GenaroNetwork/Genaro-Core/core"
+	"github.com/GenaroNetwork/Genaro-Core/params"
 )
 
 // cppEthereumGenesisSpec represents the genesis specification format used by the
