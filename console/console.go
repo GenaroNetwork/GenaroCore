a087833d8aa1188d74f4119bcd37f82e6e6355e7
lizhengxiang <411566095@qq.com>
2018-04-11 01:14:22 +0800

    update

--- console/console.go
+++ console/console.go
@@ -28,9 +28,9 @@ import (
 	"strings"
 	"syscall"
 
-	"github.com/GenaroNetwork/Genaro/internal/jsre"
-	"github.com/GenaroNetwork/Genaro/internal/web3ext"
-	"github.com/GenaroNetwork/Genaro/rpc"
+	"github.com/GenaroNetwork/Genaro-Core/internal/jsre"
+	"github.com/GenaroNetwork/Genaro-Core/internal/web3ext"
+	"github.com/GenaroNetwork/Genaro-Core/rpc"
 	"github.com/mattn/go-colorable"
 	"github.com/peterh/liner"
 	"github.com/robertkrimen/otto"
