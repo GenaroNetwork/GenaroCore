ec02e6ec623978edb46e9c2377625c537a041d26
lizhengxiang <sophia.littlefox@gmail.com>
2018-04-12 01:14:20 +0800

    update

--- accounts/abi/bind/bind.go
+++ accounts/abi/bind/bind.go
@@ -17,7 +17,7 @@
 // Package bind generates Ethereum contract Go bindings.
 //
 // Detailed usage document and tutorial available on the go-ethereum Wiki page:
-// https://github.com/GenaroNetwork/Genaro/wiki/Native-DApps:-Go-bindings-to-Ethereum-contracts
+// https://github.com/GenaroNetwork/Genaro-Core/wiki/Native-DApps:-Go-bindings-to-Ethereum-contracts
 package bind
 
 import (
@@ -28,7 +28,7 @@ import (
 	"text/template"
 	"unicode"
 
-	"github.com/GenaroNetwork/Genaro/accounts/abi"
+	"github.com/GenaroNetwork/Genaro-Core/accounts/abi"
 	"golang.org/x/tools/imports"
 )
 
