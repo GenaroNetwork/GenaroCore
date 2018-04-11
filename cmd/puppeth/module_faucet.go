ba716ed32ca03389d18a017959ed20fc13028e5c
lizhengxiang <sophia@genaro.network>
2018-04-12 01:14:21 +0800

    update

--- cmd/puppeth/module_faucet.go
+++ cmd/puppeth/module_faucet.go
@@ -26,8 +26,8 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/GenaroNetwork/Genaro/common"
-	"github.com/GenaroNetwork/Genaro/log"
+	"github.com/GenaroNetwork/Genaro-Core/common"
+	"github.com/GenaroNetwork/Genaro-Core/log"
 )
 
 // faucetDockerfile is the Dockerfile required to build an faucet container to
