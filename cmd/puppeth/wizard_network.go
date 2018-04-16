56b71c015abe2a77a6fad43d98d2e2bef04a5eda
lizhengxiang <sophia@genaro.network>
2018-04-17 00:15:20 +0800

    update

--- cmd/puppeth/wizard_network.go
+++ cmd/puppeth/wizard_network.go
@@ -20,7 +20,7 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/GenaroNetwork/Genaro/log"
+	"github.com/GenaroNetwork/Genaro-Core/log"
 )
 
 // manageServers displays a list of servers the user can disconnect from, and an
