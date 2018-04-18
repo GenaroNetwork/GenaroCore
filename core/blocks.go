061c946564344879f7260db1cab11b1b69699749
lizhengxiang <411566095@qq.com>
2018-04-19 00:15:18 +0800

    update

--- core/blocks.go
+++ core/blocks.go
@@ -16,7 +16,7 @@
 
 package core
 
-import "github.com/GenaroNetwork/Genaro/common"
+import "github.com/GenaroNetwork/Genaro-Core/common"
 
 // BadHashes represent a set of manually tracked bad hashes (usually hard forks)
 var BadHashes = map[common.Hash]bool{
