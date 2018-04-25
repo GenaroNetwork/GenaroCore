f6e04b1e79660e2515d8e232fe28b7f5cedf1b90
lizhengxiang <411566095@qq.com>
2018-04-25 20:15:19 +0800

    update

--- core/state/iterator.go
+++ core/state/iterator.go
@@ -20,9 +20,9 @@ import (
 	"bytes"
 	"fmt"
 
-	"github.com/GenaroNetwork/Genaro/common"
-	"github.com/GenaroNetwork/Genaro/rlp"
-	"github.com/GenaroNetwork/Genaro/trie"
+	"github.com/GenaroNetwork/Genaro-Core/common"
+	"github.com/GenaroNetwork/Genaro-Core/rlp"
+	"github.com/GenaroNetwork/Genaro-Core/trie"
 )
 
 // NodeIterator is an iterator to traverse the entire state trie post-order,
