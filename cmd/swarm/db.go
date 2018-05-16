abd532bdfb10f8e6a07ea39ddc33999f07dbf0e4
G-zhanghao <zhanghao@genaro.network>
2018-05-16 16:32:25 +0800

    merge and edit

--- cmd/swarm/db.go
+++ cmd/swarm/db.go
@@ -26,10 +26,6 @@ import (
 	"github.com/GenaroNetwork/Genaro-Core/log"
 	"github.com/GenaroNetwork/Genaro-Core/swarm/storage"
 	"gopkg.in/urfave/cli.v1"
-<<<<<<< HEAD
-=======
-	"github.com/mkideal/cli"
->>>>>>> origin/vm
 )
 
 func dbExport(ctx *cli.Context) {
