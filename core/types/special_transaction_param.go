7f1a5d071cac7109f86d9ad25fd487f94585e44e
eason <eason@genaro.network>
2018-06-14 15:55:30 +0800

    fmt file special_transaction_param.go

--- core/types/special_transaction_param.go
+++ core/types/special_transaction_param.go
@@ -1,15 +1,15 @@
 package types
 
 import (
-	"math/big"
 	"github.com/GenaroNetwork/Genaro-Core/common"
 	"github.com/GenaroNetwork/Genaro-Core/common/hexutil"
+	"math/big"
 )
 
 type SpecialTxInput struct {
 	GenaroData
-	NodeId  string `json:"nodeid"`
-	Type    *hexutil.Big    `json:"type"`
+	NodeId string       `json:"nodeid"`
+	Type   *hexutil.Big `json:"type"`
 }
 
 func (s SpecialTxInput) SpecialCost() *big.Int {
@@ -20,7 +20,7 @@ func (s SpecialTxInput) SpecialCost() *big.Int {
 	case common.SpecialTxTypeSpaceApply:
 		var totalCost uint64
 		for _, v := range s.Buckets {
-			oneCost := v.Size/v.Duration
+			oneCost := v.Size / v.Duration
 			totalCost += oneCost
 		}
 
@@ -30,64 +30,63 @@ func (s SpecialTxInput) SpecialCost() *big.Int {
 		totalGas := big.NewInt(int64(s.Traffic) * common.TrafficApplyGasPerG)
 		return totalGas
 	case common.SpecialTxTypeMortgageInit:
-		sumMortgageTable :=	new(big.Int)
+		sumMortgageTable := new(big.Int)
 		mortgageTable := s.SpecialTxTypeMortgageInit.MortgageTable
-		for _, v := range mortgageTable{
-			sumMortgageTable = sumMortgageTable.Add(sumMortgageTable,v.ToInt())
+		for _, v := range mortgageTable {
+			sumMortgageTable = sumMortgageTable.Add(sumMortgageTable, v.ToInt())
 		}
-		temp := s.SpecialTxTypeMortgageInit.TimeLimit.ToInt().Mul(s.SpecialTxTypeMortgageInit.TimeLimit.ToInt(),big.NewInt(int64(len(mortgageTable))))
-		timeLimitGas := temp.Mul(temp,big.NewInt(common.OneDayGes))
-		sumMortgageTable.Add(sumMortgageTable,timeLimitGas)
+		temp := s.SpecialTxTypeMortgageInit.TimeLimit.ToInt().Mul(s.SpecialTxTypeMortgageInit.TimeLimit.ToInt(), big.NewInt(int64(len(mortgageTable))))
+		timeLimitGas := temp.Mul(temp, big.NewInt(common.OneDayGes))
+		sumMortgageTable.Add(sumMortgageTable, timeLimitGas)
 		return sumMortgageTable
 	default:
 		return big.NewInt(0)
 	}
 }
 
-
 // Genaro is the Ethereum consensus representation of Genaro's data.
 // these objects are stored in the main genaro trie.
 type GenaroData struct {
-	Heft       		uint64   					`json:"heft"`
-	Stake      		uint64   					`json:"stake"`
-	Node            []string                    `json:"syncNode"`
-	SpecialTxTypeMortgageInit 	SpecialTxTypeMortgageInit	`json:"specialTxTypeMortgageInit"`
-	SpecialTxTypeMortgageInitArr 	map[string]SpecialTxTypeMortgageInit	`json:"specialTxTypeMortgageInitArr"`
-	Traffic         uint64                      `json:"traffic"`
-	Buckets         []*BucketPropertie	`json:"buckets"`
+	Heft                         uint64                               `json:"heft"`
+	Stake                        uint64                               `json:"stake"`
+	Node                         []string                             `json:"syncNode"`
+	SpecialTxTypeMortgageInit    SpecialTxTypeMortgageInit            `json:"specialTxTypeMortgageInit"`
+	SpecialTxTypeMortgageInitArr map[string]SpecialTxTypeMortgageInit `json:"specialTxTypeMortgageInitArr"`
+	Traffic                      uint64                               `json:"traffic"`
+	Buckets                      []*BucketPropertie                   `json:"buckets"`
 }
 
 type BucketPropertie struct {
-	BucketId         string `json:"bucketId"`
+	BucketId string `json:"bucketId"`
 
 	// 开始时间和结束时间共同表示存储空间的时长，对应STORAGEGAS指令
-	TimeStart        uint64	`json:"timeStart"`
-	TimeEnd          uint64	`json:"timeEnd"`
-	Duration         uint64 `json:"duration"`
+	TimeStart uint64 `json:"timeStart"`
+	TimeEnd   uint64 `json:"timeEnd"`
+	Duration  uint64 `json:"duration"`
 
 	// 备份数，对应STORAGEGASPRICE指令
-	Backup           uint64 `json:"backup"`
+	Backup uint64 `json:"backup"`
 
 	// 存储空间大小，对应SSIZE指令
-	Size             uint64 `json:"size"`
+	Size uint64 `json:"size"`
 }
 
-type Sidechain 	map[common.Address] *hexutil.Big
+type Sidechain map[common.Address]*hexutil.Big
 
 type FileIDArr struct {
-	MortgageTable	map[common.Address] *hexutil.Big	`json:"mortgage"`
-	AuthorityTable 	map[common.Address]int	`json:"authority"`
-	FileID 			string		`json:"fileID"`
-	Dataversion		string		`json:"dataversion"`
-	SidechainStatus	map[string] map[common.Address] *hexutil.Big	`json:"sidechainStatus"`
-	MortgagTotal	*big.Int	`json:"MortgagTotal"`
-	LogSwitch 	bool	`json:"logSwitch"`
-	TimeLimit   *hexutil.Big `json:"timeLimit"`
-	CreateTime  int64	`json:"createTime"`
-	EndTime  int64	`json:"endTime"`
-	FromAccount common.Address 	`json:"fromAccount"`
-	Terminate	bool			`json:"terminate"`
-	Sidechain		Sidechain `json:"sidechain"`
+	MortgageTable   map[common.Address]*hexutil.Big            `json:"mortgage"`
+	AuthorityTable  map[common.Address]int                     `json:"authority"`
+	FileID          string                                     `json:"fileID"`
+	Dataversion     string                                     `json:"dataversion"`
+	SidechainStatus map[string]map[common.Address]*hexutil.Big `json:"sidechainStatus"`
+	MortgagTotal    *big.Int                                   `json:"MortgagTotal"`
+	LogSwitch       bool                                       `json:"logSwitch"`
+	TimeLimit       *hexutil.Big                               `json:"timeLimit"`
+	CreateTime      int64                                      `json:"createTime"`
+	EndTime         int64                                      `json:"endTime"`
+	FromAccount     common.Address                             `json:"fromAccount"`
+	Terminate       bool                                       `json:"terminate"`
+	Sidechain       Sidechain                                  `json:"sidechain"`
 }
 
 //Cross-chain storage processing
