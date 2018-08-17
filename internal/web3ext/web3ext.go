17e62bac5432558872cdb2c8d8e8ebef90a47e5a
lizhengxiang <1065482100@qq.com>
2018-08-17 03:18:28 -0400

    web 3 api

--- internal/web3ext/web3ext.go
+++ internal/web3ext/web3ext.go
@@ -540,6 +540,18 @@ web3._extend({
 			params: 2,
 			inputFormatter: [web3._extend.formatters.inputAddressFormatter,web3._extend.formatters.inputString]
 		}),
+		new web3._extend.Method({
+			name: 'getAllPromissoryNotesNum',
+			call: 'eth_getAllPromissoryNotesNum',
+			params: 1,
+			inputFormatter: [web3._extend.formatters.inputAddressFormatter]
+		}),
+		new web3._extend.Method({
+			name: 'getBeforPromissoryNotesNum',
+			call: 'eth_getBeforPromissoryNotesNum',
+			params: 1,
+			inputFormatter: [web3._extend.formatters.inputAddressFormatter]
+		}),
 	],
 	properties: [
 		new web3._extend.Property({
