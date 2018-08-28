bb25e137dc753b52778bbd0e169a84d7f4236d7a
G-zhanghao <zhanghao@genaro.network>
2018-08-28 17:21:16 +0800

    update app info

--- cmd/go-genaro/main.go
+++ cmd/go-genaro/main.go
@@ -28,7 +28,6 @@ import (
 	"github.com/GenaroNetwork/Genaro-Core/accounts"
 	"github.com/GenaroNetwork/Genaro-Core/accounts/keystore"
 	"github.com/GenaroNetwork/Genaro-Core/cmd/utils"
-	"github.com/GenaroNetwork/Genaro-Core/common"
 	"github.com/GenaroNetwork/Genaro-Core/console"
 	"github.com/GenaroNetwork/Genaro-Core/eth"
 	"github.com/GenaroNetwork/Genaro-Core/ethclient"
@@ -47,9 +46,9 @@ var (
 	// Git SHA1 commit hash of the release (set via linker flags)
 	gitCommit = ""
 	// Ethereum address of the Geth release oracle.
-	relOracle = common.HexToAddress("0xfa7b9770ca4cb04296cac84f37736d4041251cdf")
+	//relOracle = common.HexToAddress("0xfa7b9770ca4cb04296cac84f37736d4041251cdf")
 	// The app that holds all commands and flags.
-	app = utils.NewApp(gitCommit, "the go-ethereum command line interface")
+	app = utils.NewApp(gitCommit, "the genero command line interface")
 	// flags that configure the node
 	nodeFlags = []cli.Flag{
 		utils.IdentityFlag,
@@ -149,7 +148,7 @@ func init() {
 	// Initialize the CLI app and start Geth
 	app.Action = geth
 	app.HideVersion = true // we have a command to print the version
-	app.Copyright = "Copyright 2013-2017 The go-ethereum Authors"
+	app.Copyright = "Copyright 2013-2017 The go-ethereum Authors and 2018 The go-genaro Authors"
 	app.Commands = []cli.Command{
 		// See chaincmd.go:
 		initCommand,
