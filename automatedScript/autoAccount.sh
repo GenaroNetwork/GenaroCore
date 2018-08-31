cb87a5f45767912c1eb74263e2f79ecd6436cc96
lizhengxiang <1065482100@qq.com>
2018-08-31 02:26:22 -0400

    del automatedScript

--- automatedScript/autoAccount.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
- 
-for i in $(seq 1 150)
-do 
-curl -X POST -H 'content-type: application/json' --data '{"jsonrpc":"2.0","method":"personal_newAccount","params":["123456"],"id":1}' "http://127.0.0.1:8545"
-done
