67c5d6ec6b77c02a4686a2a55af0daefdafe2a73
G-zhanghao <zhanghao@genaro.network>
2019-06-25 10:05:10 +0800

    update version

--- params/version.go
+++ params/version.go
@@ -22,9 +22,9 @@ import (
 
 const (
 	VersionMajor = 1     // Major version component of the current release
-	VersionMinor = 0     // Minor version component of the current release
-	VersionPatch = 3     // Patch version component of the current release
-	VersionMeta  = "dev" // Version metadata to append to the version string
+	VersionMinor = 1     // Minor version component of the current release
+	VersionPatch = 0     // Patch version component of the current release
+	VersionMeta  = "rel" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
