c26dc128358da93cac1b0d0e0d839b090f4638c9
G-zhanghao <zhanghao@genaro.network>
2019-10-12 17:53:19 +0800

    update levlog

--- common/levlog/levlog.go
+++ common/levlog/levlog.go
@@ -2,6 +2,7 @@ package levlog
 
 import (
 	"github.com/syndtr/goleveldb/leveldb"
+	"encoding/binary"
 	"sync"
 )
 
@@ -10,6 +11,16 @@ var (
 	FIR_INDEX_B = []byte("FIR_INDEX")
 )
 
+func Int64ToBytes(i int64) []byte {
+	var buf = make([]byte, 8)
+	binary.BigEndian.PutUint64(buf, uint64(i))
+	return buf
+}
+
+func BytesToInt64(buf []byte) int64 {
+	return int64(binary.BigEndian.Uint64(buf))
+}
+
 type Levlog struct {
 	Dbdir      string
 	DB         *leveldb.DB
@@ -17,3 +28,4 @@ type Levlog struct {
 	FirstIndex int64
 	NowIndex   int64
 }
+
