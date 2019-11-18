22ffd40ee9f1045637073244d7cda5cd7b3b300a
G-zhanghao <zhanghao@genaro.network>
2019-11-18 11:35:22 +0800

    update GetLog

--- common/datalog.go
+++ common/datalog.go
@@ -1,18 +1,24 @@
 package common
 
-import "mypkg/levlog"
+import "github.com/GenaroNetwork/GenaroCore/common/levlog"
 
 var datalog *levlog.Levlog
 var logdir string
 
-func InitDir(dir string) {
+func InitDataLogDir(dir string) {
 	if logdir == "" {
 		logdir = dir
 	}
 }
 
-func GetLog() {
+func GetLog() (*levlog.Levlog, error) {
 	if datalog == nil {
-
+		dlog, err := levlog.GenLevlog(logdir)
+		if err != nil {
+			return nil, err
+		} else {
+			datalog = dlog
+		}
 	}
+	return datalog, nil
 }
